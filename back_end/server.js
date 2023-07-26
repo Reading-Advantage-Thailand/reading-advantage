a1c9352bbe1522206d7e6e322f2ecad015a6125d
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-07-26 09:28:47 +0700

    [DELETE] old backend

--- back_end/server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-const app = require('./app');
-const port = process.env.PORT || 3000;
-dotenv.config({ path: './config.env' });
-
-const server = app.listen(process.env.PORT, () => {
-    console.log(`App running on port ${port}...`);
-});
-
--- back_end/utils/catchAsync.js
+++
@@ -1,6 +0,0 @@
-// Use to catch async errors
-module.exports = fn => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next);
-    }
-}
