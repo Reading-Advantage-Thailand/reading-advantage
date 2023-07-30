b760f790a2d5e63c8fc509ffe9c00f0eac792395
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-07-30 20:55:19 +0700

    [DELETE] client and server
    combined in nextJS

--- server/src/config/serverConfig.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export default {
-    baseUrl: `${process.env.BASE_URL}${process.env.PORT}/api/${process.env.API_VERSION}` || 'http://localhost:8000/api/v1',
-    port: process.env.PORT || 8000,
-    apiVersion: process.env.API_VERSION || 'v1',
-    nodeENV: process.env.NODE_ENV || 'development',
-};
