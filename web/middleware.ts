7e1f55044e711edb27dd3058ed562a7ae1bccf84
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-08-06 08:34:13 +0700

    [ADD] middleware to protect the required authentication reroute

---
+++ web/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+    const path = req.nextUrl.pathname;
+    const isPublic = ['/', '/auth'].includes(path);
+    const token = req.cookies.get('token')?.value || '';
+
+    if (isPublic && token) {
+        return NextResponse.redirect(new URL('/', req.nextUrl));
+    }
+
+    if (!isPublic && !token) {
+        return NextResponse.redirect(new URL('/auth', req.nextUrl));
+    }
+}
+
+export const config = {
+    matcher: [
+        '/',
+        '/auth',
+        '/start',
+    ]
+}
