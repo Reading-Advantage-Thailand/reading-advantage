03ec9f366591ffa68acf1cb57057b02944dd07e6
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-09-26 00:51:56 +0700

    [UPDATE] theme provider and theme switcher

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,47 +1,46 @@
 import type { Metadata } from 'next'
-import { NextAuthProvider } from './provider'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@lib/auth'
-import { bg } from '@constants/colors'
+import PageProvider from '../components/helpers/page-provider';
+import { EmotionCache } from "@emotion/react";
+import ThemeSwitcher from '@components/theme/theme-switcher'
+import { NextAuthProvider } from '@components/helpers/auth-provider';
 
 export const metadata: Metadata = {
   title: 'Reading Advantage',
   description: 'Extensive reading app incorporating AI.',
+  keywords: ['extensive reading', 'language learning', 'ai', 'artificial intelligence', 'reading', 'reading advantage', 'readingadvantage', 'readingadvantage.com'],
+  authors: [
+    {
+      name: 'Reading Advantage',
+      url: 'https://app.reading-advantage.com',
+    },
+  ],
   icons: {
-    icon: [
-      '/favicon.ico?v=4',
-    ],
-    apple: [
-      '/apple-touch-icon.png?v=4'
-    ],
-    shortcut: [
-      '/apple-touch-icon.png',
-    ],
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png'],
   },
   manifest: '/site.webmanifest',
 }
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session = await getServerSession(authOptions)
+interface RootLayoutProps {
+  children: React.ReactNode
+  emotionCache?: EmotionCache
+}
 
+export default async function RootLayout({ children, emotionCache }: RootLayoutProps) {
+  const session = await getServerSession(authOptions);
   return (
-    <html lang="en">
-      <body
-        style={{
-          // backgroundColor: '#f5f5f5',
-          margin: 0,
-          padding: 0,
-          backgroundColor: bg,
-        }}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body>
         <NextAuthProvider session={session}>
+          {/* <PageProvider emotionCache={emotionCache}> */}
+          {/* <ThemeSwitcher /> */}
           {children}
+          {/* </PageProvider> */}
         </NextAuthProvider>
       </body>
     </html>
-  );
+  )
 }
