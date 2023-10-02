8b7f93de89cf10b10ccea00287b61433247a6eca
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-10-02 21:08:29 +0700

    delete: old web folder

--- web/lib/session.ts
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@lib/auth";
-
-// Get current session from the server
-// This is used to get the current user in the server side
-export async function getCurrentUser() {
-    const session = await getServerSession(authOptions);
-    return session?.user;
-}
--- web/models/userModel.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id: string;
-    username: string;
-    email: string;
-    level: number;
-    fLang: string;
-    image: string;
-}
