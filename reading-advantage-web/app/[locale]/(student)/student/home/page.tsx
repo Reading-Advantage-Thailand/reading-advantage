546a3e9426293f07f13d5682c2c83c36748c2318
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-10-07 02:06:02 +0700

    fix: auth

---
+++ reading-advantage-web/app/[locale]/(student)/student/home/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function StudentHomePage({ }: Props) {
+    return (
+        <div>StudentHomePage</div>
+    )
+}
---
+++ reading-advantage-web/components/user-auth-form.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { signIn } from "next-auth/react"
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
+
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+    const [isGoogleLoading, setIsGoogleLoading] = React.useState<boolean>(false)
+    return (
+        <div className={cn("grid gap-6", className)} {...props}>
+            <button
+                type="button"
+                className={cn(buttonVariants({ variant: "outline" }))}
+                onClick={() => {
+                    setIsGoogleLoading(true)
+                    try {
+                        signIn("google", {
+                            callbackUrl: `${window.location.origin}/student/home`,
+                        })
+                    } catch (error) {
+                        console.log(error)
+                    }
+
+                }}
+                disabled={isGoogleLoading}
+            >
+                {isGoogleLoading ? (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                    <Icons.google className="mr-2 h-4 w-4" />
+                )}{" "}
+                Google
+            </button>
+        </div>
+    )
+}
