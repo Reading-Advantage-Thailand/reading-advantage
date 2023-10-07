18233dd07349a0c4aabf061914572882f682a2d4
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-10-07 17:43:05 +0700

    delete: analytic.tsx

--- reading-advantage-web/components/analytics.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { Analytics as VercelAnalytics } from "@vercel/analytics/react"
-
-export function Analytics() {
-    const mode = process.env.NODE_ENV === "production" ? "production" : "development";
-    return <VercelAnalytics mode={mode} />
-}
