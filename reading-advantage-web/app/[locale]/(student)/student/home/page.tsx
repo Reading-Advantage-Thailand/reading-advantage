464869e3512ad1adb77ee2dbb70b0873fe70aa31
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-10-07 16:06:15 +0700

    fix: env

--- reading-advantage-web/app/[locale]/(student)/student/home/page.tsx
+++ reading-advantage-web/app/[locale]/(student)/student/home/page.tsx
@@ -8,15 +8,10 @@ type Props = {}
 
 export default async function StudentHomePage({ }: Props) {
     const session = await getCurrentUser();
-    const getTest = await axios.get('http://localhost:3000/api/test');
-    const test = getTest.data;
     return (
         <>
             <div className='mt-10'>
                 {session?.name}
-            </div>
-            <div>
-                {test && JSON.stringify(test)}
             </div>
             <ButtonLogout />
         </>
--- reading-advantage-web/app/api/test/route.ts
+++ reading-advantage-web/app/api/test/route.ts
@@ -4,9 +4,18 @@ import { NextResponse } from "next/server";
 export async function GET(req: Request, res: Response) {
 
     const service_account = process.env.SERVICE_ACCOUNT_KEY;
+    const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+    const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+    const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
+
+    const NEXT_PUBLIC_URL_BASE = process.env.NEXT_PUBLIC_URL_BASE;
     return new Response(JSON.stringify({
         message: "hello",
         service_account,
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET,
+        NEXTAUTH_SECRET,
+        NEXT_PUBLIC_URL_BASE,
     }), {
         headers: {
             "content-type": "application/json",
