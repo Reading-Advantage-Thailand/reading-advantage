efbdd4214e7559a282f0794ef56037867ba68d5f
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-10-11 16:54:09 +0700

    delete: the old website

--- web/app/[locale]/(index)/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-export default function ContactPage({ }: Props) {
-    return (
-        <div>please contact: Daniel Bo email: admin@reading-advantage.com</div>
-    )
-}
