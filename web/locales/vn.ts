25cf29e0dc37fa0c87c176d872492d33ef77638a
bodangren <bodangren@gmail.com>
2023-11-07 13:21:59 +0800

    Create vn.ts

---
+++ web/locales/vn.ts
@@ -0,0 +1,164 @@
+{
+  "pages": {
+    "indexPage": {
+      "description": "Ứng dụng đọc rộng rãi tích hợp AI. Phiên bản Beta và đang trong quá trình thử nghiệm.",
+      "getStartedButton": "Bắt đầu"
+    },
+    "loginPage": {
+      "loginDescription": "Đăng nhập vào tài khoản của bạn",
+      "backButton": "Quay lại"
+    },
+    "student": {
+      "homePage": {
+        "reminderToReread": "Nhắc nhở đọc lại",
+        "articleRecords": "Hồ sơ Bài báo",
+        "reminderToRereadDescription": "Có thể bạn muốn đọc một số bài báo này lần nữa để xem bạn đã tiến bộ chưa.",
+        "articleRecordsDescription": "Hồ sơ đọc của bạn sẽ được hiển thị ở đây."
+      },
+      "nextQuizPage": {
+        "articleSelection": "Lựa chọn Bài báo",
+        "article": {
+          "articleNotFound": "Không tìm thấy bài báo",
+          "articleInsufficientLevel": "Bạn không thể đọc bài báo này do trình độ đọc của bạn không đủ.",
+          "readBefore": "Bạn đã đọc bài báo này trước đây",
+          "readBeforeDescription": "Bạn có thể thử đọc và luyện tập lại để cải thiện kỹ năng đọc của mình. Đây là kết quả của lần đọc trước.",
+          "status": "Trạng thái",
+          "statusText": {
+            "completed": "Hoàn thành",
+            "uncompleted": "Chưa hoàn thành"
+          },
+          "statusDescription": "Cập nhật lần cuối vào {date}",
+          "score": "Điểm Quiz",
+          "scoreText": "{score}",
+          "scoreDescription": "từ {total} câu hỏi",
+          "rated": "Đánh giá",
+          "ratedText": "{rated} đánh giá",
+          "ratedDescription": "Bạn đã đánh giá bài báo này",
+          "timeSpend": "Thời gian đã dành",
+          "timeSpendText": "{time}",
+          "timeSpendDescription": "trong {total} câu hỏi",
+          "scoreSuffix": {
+            "point": "điểm",
+            "points": "điểm"
+          },
+          "secondSuffix": {
+            "second": "giây",
+            "seconds": "giây"
+          }
+        }
+      },
+      "flashcardPage": {
+        "flashcard": "Thẻ ghi nhớ",
+        "flashcardDescription": "Bạn có thể luyện tập kỹ năng đọc của mình với thẻ ghi nhớ và câu bạn đã lưu sẽ được hiển thị ở đây.",
+        "savedSentences": "Câu đã lưu",
+        "noSavedSentences": "Bạn không có câu nào được lưu.",
+        "savedSentencesDescription": "Bạn có {total} câu đã lưu.",
+        "added": "Đã thêm vào {date}",
+        "deleteButton": "Xóa",
+        "toast": {
+          "success": "Thành công",
+          "successDescription": "Xóa câu đã lưu thành công",
+          "error": "Lỗi",
+          "errorDescription": "Đã có lỗi xảy ra khi xóa câu đã lưu"
+        }
+      }
+    }
+  },
+  "components": {
+    "mainNav": {
+      "home": "Trang chủ",
+      "about": "Về chúng tôi",
+      "contact": "Liên hệ"
+    },
+    "userAccountNav": {
+      "level": "Cấp độ {level}",
+      "settings": "Cài đặt",
+      "signOut": "Đăng xuất"
+    },
+    "themeSwitcher": {
+      "dark": "Tối",
+      "light": "Sáng",
+      "system": "Hệ thống"
+    },
+    "localeSwitcher": {
+      "en": "Tiếng Anh",
+      "th": "Tiếng Thái",
+      "cn": "Tiếng Trung (Giản thể)",
+      "tw": "Tiếng Trung (Phồn thể)"
+    },
+    "sidebarNav": {
+      "articleRecords": "Hồ sơ Bài báo",
+      "nextQuiz": "Quiz tiếp theo",
+      "flashcard": "Thẻ ghi nhớ"
+    },
+    "articleRecordsTable": {
+      "title": "Tiêu đề",
+      "date": "Ngày",
+      "rated": "Đánh giá",
+      "search": "Tìm kiếm tiêu đề...",
+      "previous": "trước",
+      "next": "tiếp theo",
+      "select": "{selected} trong tổng số {total} được chọn"
+    },
+    "select": {
+      "articleChoose": "Vui lòng chọn {article} bạn muốn đọc",
+      "articleChooseDescription": "Trình độ của bạn là {level} và đây là các {article} bạn có thể chọn."
+    },
+    "article": {
+      "type": "loại bài báo",
+      "genre": "thể loại",
+      "subGenre": "tiểu thể loại"
+    },
+    "articleCard": {
+      "raLevel": "Mức độ đọc là {raLevel}",
+      "cefrLevel": "Mức CEFR là {cefrLevel}",
+      "articleCardDescription": "Bài viết liên quan đến chủ đề của {topic}, nằm trong thể loại {genre}."
+    },
+    "articleContent": {
+      "voiceAssistant": "Trợ lý giọng nói",
+      "soundButton": {
+        "play": "Phát âm thanh",
+        "pause": "Tạm dừng"
+      }
+    },
+    "mcq": {
+      "quiz": "Quiz",
+      "reQuiz": "Làm Quiz lại",
+      "quizDescription": "Bắt đầu quiz để kiểm tra kiến thức của bạn và xem bài viết này dễ đối với bạn như thế nào.",
+      "reQuizDescription": "Bạn đã hoàn thành quiz này trước đây. Bạn có thể làm lại để cải thiện điểm số của mình.",
+      "startButton": "Bắt đầu Quiz",
+      "retakeButton": "Làm lại Quiz",
+      "elapsedTime": "{time} giây đã trôi qua",
+      "questionHeading": "Câu hỏi {number} trong tổng số {total}",
+      "nextQuestionButton": "Câu hỏi tiếp theo",
+      "toast": {
+        "correct": "Đúng",
+        "correctDescription": "Bạn đã trả lời đúng!",
+        "incorrect": "Sai",
+        "incorrectDescription": "Bạn đã trả lời sai!",
+        "quizCompleted": "Hoàn thành Quiz",
+        "quizCompletedDescription": "Bạn đã hoàn thành quiz",
+        "error": "Lỗi",
+        "errorDescription": "Đã có lỗi xảy ra khi nộp câu trả lời của bạn. Vui lòng thử lại sau."
+      }
+    },
+    "rate": {
+      "title": "Đánh giá bài viết này",
+      "description": "Đánh giá này được sử dụng để tính toán cấp độ tiếp theo của bạn.",
+      "newLevel": "Cấp độ mới của bạn là {level}",
+      "submitButton": "Nộp",
+      "backToHomeButton": "Trở về Trang chủ",
+      "nextQuizButton": "Quiz tiếp theo",
+      "toast": {
+        "success": "Thành công",
+        "successDescription": "Cấp độ mới của bạn là {level}."
+      }
+    },
+    "audioButton": {
+      "play": "Phát âm thanh",
+      "pause": "Tạm dừng"
+    },
+    "menu": "Menu",
+    "loginButton": "Đăng nhập"
+  }
+}
