16608c879f029df325540671474365483aa2ec5d
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-12-22 13:52:52 +0700

    fix: authentication
    - OAuth
    - Sign in, Sign up and forgot password pages

--- web/lib/auth.ts
+++ web/lib/auth.ts
@@ -1,54 +1,76 @@
 import db from "@/configs/firestore-config"
 import { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { firebaseAdmin } from "./firebaseAdmin"
 
 export const authOptions: NextAuthOptions = {
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        CredentialsProvider({
+            credentials: {},
+            authorize: async (credentials: { idToken?: string } | undefined, _req) => {
+                if (credentials && credentials.idToken) {
+                    try {
+                        const decoded = await firebaseAdmin.auth().verifyIdToken(credentials.idToken);
+                        // console.log('decoded', decoded);
+                        // Convert the decoded token to a User object
+                        const user = {
+                            id: decoded.uid,
+                            name: decoded.name,
+                            email: decoded.email,
+                            image: decoded.picture,
+                        };
+                        console.log('user', user);
+                        return user;
+                    } catch (err) {
+                        console.error(err);
+                        return Promise.resolve(null);
+                    }
+                }
+                return Promise.resolve(null);
+            },
         }),
     ],
     session: {
         strategy: "jwt",
     },
     pages: {
-        'signIn': '/login',
+        'signIn': '/auth/signin',
     },
     jwt: {
         secret: process.env.NEXTAUTH_SECRET,
     },
     callbacks: {
         jwt: async ({ token }) => {
-            // console.log("jwt callback");
-            // console.log('token', token);
-            const user = await db.collection("users")
-                .doc(token.sub as string)
-                .get();
-            const userData = user.data();
-
-            if (userData) {
-                token.id = userData.id;
-                token.name = userData.name;
-                token.email = userData.email;
-                token.picture = userData.picture;
-                token.level = userData.level;
-            }
-            // create account if it doesn't exist
-            else {
-                await db.collection("users")
+            try {
+                console.log('token', token);
+                const user = await db.collection("users")
                     .doc(token.sub as string)
-                    .set({
-                        id: token.sub,
-                        name: token.name,
-                        email: token.email,
-                        picture: token.picture,
-                        createAt: new Date(),
-                        lastLogin: new Date(),
-                        level: 0,
-                    });
+                    .get();
+                const userData = user.data();
+                if (userData) {
+                    token.id = userData.id;
+                    token.name = userData.name;
+                    token.email = userData.email;
+                    token.picture = userData.picture;
+                    token.level = userData.level;
+                }
+                // create account if it doesn't exist
+                else {
+                    await db.collection("users")
+                        .doc(token.sub as string)
+                        .set({
+                            id: token.sub,
+                            name: token.name,
+                            email: token.email,
+                            picture: token.picture,
+                            createAt: new Date(),
+                            level: 0,
+                        });
+                }
+                return token;
+            } catch (error) {
+                return Promise.reject(error);
             }
-            return token;
         },
         session: ({ session, token }) => {
             if (token) {
@@ -62,5 +84,5 @@ export const authOptions: NextAuthOptions = {
             console.log('session', session);
             return session;
         },
-    },
+    }
 };
