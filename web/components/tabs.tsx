a9564b7009bd6ffce68cf8bb3d20fa16e9002f4a
rtclub11140 <saeree_club@hotmail.co.th>
2024-01-09 09:10:34 +0700

    feat: add component tabs

---
+++ web/components/tabs.tsx
@@ -0,0 +1,32 @@
+/*
+return (
+      <Tabs.Root className="TabsRoot" defaultValue="tab1">
+        <Tabs.List className="TabsList" aria-label="Flashcard Practice">
+          <Tabs.Trigger className="TabsTrigger" value="tab1">
+            Flashcard Practice
+          </Tabs.Trigger>
+          <Tabs.Trigger className="TabsTrigger" value="tab2">
+            Order Sentences
+          </Tabs.Trigger>
+          <Tabs.Trigger className="TabsTrigger" value="tab3">
+            Close Test
+          </Tabs.Trigger>
+          <Tabs.Trigger className="TabsTrigger" value="tab4">
+            Order Words
+          </Tabs.Trigger>
+          <Tabs.Trigger className="TabsTrigger" value="tab5">
+            Matching
+          </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content className="TabsContent" value="tab1">
+          <FlashCard userId={user.id} />
+        </Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab2"></Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab3"></Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab4"></Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab5"></Tabs.Content>
+      </Tabs.Root>
+      //
+    );
+
+*/
