9c59f9f95efe69b63516089cec9260f7134b0bb5
rtclub11140 <saeree_club@hotmail.co.th>
2024-01-15 11:46:31 +0700

    feat: edit example ts-fsrs

--- web/components/flash-card-practice-button.tsx
+++ web/components/flash-card-practice-button.tsx
@@ -30,118 +30,161 @@ export default function FlashCardPracticeButton({
   sentences,
 }: Props) {
   const t = useScopedI18n("pages.student.practicePage");
-  const params = generatorParameters({ enable_fuzz: true });
-  let now = new Date();
-  const startOfDay = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate(),
-    4,
-    0,
-    0,
-    0
-  );
-  let card: Card = createEmptyCard();
-  const f: FSRS = fsrs(params);
-  let scheduling_cards: RecordLog = f.repeat(card, startOfDay);
+  console.log("==> sentences", sentences);
+  const [review, setReview] = React.useState(new Date());
+  const [cards, setCards] = React.useState([createEmptyCard()] || []);
+  const [logs, setLogs] = React.useState<any>([]);
+  // const params = generatorParameters({ enable_fuzz: true });
+  // let now = new Date();
+  // const startOfDay = new Date(
+  //   now.getFullYear(),
+  //   now.getMonth(),
+  //   now.getDate(),
+  //   4,
+  //   0,
+  //   0,
+  //   0
+  // );
+  // let card: Card = createEmptyCard();
 
-  const handleClickFsrs = async (index: number, level: string) => {
-    if (level === "Again") {
-      scheduling_cards[Rating.Again].card;
-      scheduling_cards[Rating.Again].log;
-    } else if (level === "Hard") {
-      scheduling_cards[Rating.Hard].card;
-      scheduling_cards[Rating.Hard].log;
-    } else if (level === "Good") {
-      scheduling_cards[Rating.Good].card;
-      scheduling_cards[Rating.Good].log;
-    } else if (level === "Easy") {
-      scheduling_cards[Rating.Easy].card;
-      scheduling_cards[Rating.Easy].log;
-    }
-    // console.log("sentences :>> ", sentences[index]);
-    // scheduling_cards[Rating.Again].card
-    // scheduling_cards[Rating.Again].log;
-    // console.log("scheduling_cards :>> ", scheduling_cards);
-    Grades.forEach((grade) => {
-      // [Rating.Again, Rating.Hard, Rating.Good, Rating.Easy]
-      const { log, card } = scheduling_cards[grade];
-      console.group(`${Rating[grade]}`);
-      console.table({
-        [`card_${Rating[grade]}`]: {
-          ...card,
-          due: formatDate(card.due),
-          last_review: formatDate(card.last_review as Date),
-        },
-      });
-      console.table({
-        [`log_${Rating[grade]}`]: {
-          ...log,
-          review: formatDate(log.review),
-        },
-      });
-      console.groupEnd();
-      console.log(
-        "----------------------------------------------------------------"
-      );
-    });
+  const params = generatorParameters();
+  const f: FSRS = fsrs(params);
+  // let scheduling_cards: RecordLog = f.repeat(card, startOfDay);
+
+  const handleClickFsrs = async (index: number, rating: Rating) => {
+    console.log("==> cards", cards.length);
+    console.log(Rating[rating]);
+    const preCard: any =
+      cards.length > 0 ? cards[cards.length - 1] : createEmptyCard(new Date());
+    const scheduling_cards: any = f.repeat(preCard, preCard.due);
+    console.log(scheduling_cards);
+    setCards((pre) => [...pre, scheduling_cards[rating].card]);
+    setLogs((pre: any) => [...pre, scheduling_cards[rating].log]);
   };
 
   return (
-    <div className="flex space-x-2">
-      <p>{State.Learning}</p><br/>
-      <button
-        className={cn(
-          buttonVariants({ size: "sm" }),
-          "bg-red-500",
-          "hover:bg-red-600"
-        )}
-        onClick={() => {
-          handleClickFsrs(index, "Again");
-          nextCard();
-        }}
-      >
-        {t("flashcardPractice.buttonAgain")}
-      </button>
-      <button
-        className={cn(
-          buttonVariants({ size: "sm" }),
-          "bg-amber-500",
-          "hover:bg-amber-600"
-        )}
-        onClick={() => {
-          handleClickFsrs(index, "Hard");
-          nextCard();
-        }}
-      >
-        {t("flashcardPractice.buttonHard")}
-      </button>
-      <button
-        className={cn(
-          buttonVariants({ size: "sm" }),
-          "bg-emerald-500",
-          "hover:bg-emerald-600"
-        )}
-        onClick={() => {
-          handleClickFsrs(index, "Good");
-          nextCard();
-        }}
-      >
-        {t("flashcardPractice.buttonGood")}
-      </button>
-      <button
-        className={cn(
-          buttonVariants({ size: "sm" }),
-          "bg-blue-500",
-          "hover:bg-blue-600"
-        )}
-        onClick={() => {
-          handleClickFsrs(index, "Easy");
-          nextCard();
-        }}
-      >
-        {t("flashcardPractice.buttonEasy")}
-      </button>
-    </div>
+    <>
+      <div className="flex space-x-2">
+        <button
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "bg-red-500",
+            "hover:bg-red-600"
+          )}
+          onClick={() => {
+            handleClickFsrs(index, Rating.Again);
+            nextCard();
+          }}
+        >
+          {t("flashcardPractice.buttonAgain")}
+        </button>
+        <button
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "bg-amber-500",
+            "hover:bg-amber-600"
+          )}
+          onClick={() => {
+            handleClickFsrs(index, Rating.Hard);
+            nextCard();
+          }}
+        >
+          {t("flashcardPractice.buttonHard")}
+        </button>
+        <button
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "bg-emerald-500",
+            "hover:bg-emerald-600"
+          )}
+          onClick={() => {
+            handleClickFsrs(index, Rating.Good);
+            nextCard();
+          }}
+        >
+          {t("flashcardPractice.buttonGood")}
+        </button>
+        <button
+          className={cn(
+            buttonVariants({ size: "sm" }),
+            "bg-blue-500",
+            "hover:bg-blue-600"
+          )}
+          onClick={() => {
+            handleClickFsrs(index, Rating.Easy);
+            nextCard();
+          }}
+        >
+          {t("flashcardPractice.buttonEasy")}
+        </button>
+      </div>
+      <div className="pt-4">Next review: {review.toLocaleString()}</div>
+      <div className="pt-4">Cards:</div>
+      <table>
+        <thead>
+          <tr>
+            <th>index</th>
+            <th>due</th>
+            <th>state</th>
+            <th>last_review</th>
+            <th>stability</th>
+            <th>difficulty</th>
+            <th>R</th>
+            <th>elapsed_days</th>
+            <th>scheduled_days</th>
+            <th>reps</th>
+            <th>lapses</th>
+          </tr>
+        </thead>
+        <tbody className="text-sm text-center">
+          {cards.map((record, index) => (
+            <tr className="hover:bg-zinc-200" key={index}>
+              <td>{index + 1}</td>
+              <td>{record.due.toLocaleString()}</td>
+              <td>{`${record.state}(${State[record.state]})`}</td>
+              <td>
+                {record.last_review && record.last_review.toLocaleString()}
+              </td>
+              <td>{record.stability.toFixed(2)}</td>
+              <td>{record.difficulty.toFixed(2)}</td>
+              <td>{f.get_retrievability(record, record.due) || "/"}</td>
+              <td>{record.elapsed_days}</td>
+              <td>{record.scheduled_days}</td>
+              <td>{record.reps}</td>
+              <td>{record.lapses}</td>
+            </tr>
+          ))}
+          <tr></tr>
+        </tbody>
+      </table>
+      <div className="pt-4">logRecord:</div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>rating</th>
+            <th>state</th>
+            <th>due</th>
+            <th>elapsed_days</th>
+            <th>scheduled_days</th>
+            <th>review</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((record: any, index: any) => (
+            <tr className="hover:bg-zinc-200" key="index">
+              <th>{"=>"}</th>
+              <td>{`${record.rating}(${Rating[record.rating]})`}</td>
+              <td>{`${record.state}(${State[record.state]})`}</td>
+              <td>{record.due.toLocaleString()}</td>
+              <td>{record.elapsed_days}</td>
+              <td>{record.scheduled_days}</td>
+              <td>{record.review.toLocaleString()}</td>
+            </tr>
+          ))}
+          <tr></tr>
+        </tbody>
+      </table>
+    </>
   );
 }
