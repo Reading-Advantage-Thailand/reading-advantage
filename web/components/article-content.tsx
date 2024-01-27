cb7ad7d4f128ed6a749b5ce4372e33976c87f851
saeree <saeree_club@hotmail.co.th>
2024-01-27 09:26:10 +0700

    fix: clear log

--- web/components/article-content.tsx
+++ web/components/article-content.tsx
@@ -173,8 +173,6 @@ export default function ArticleContent({
           ...card,
         });
 
-        console.log(res);
-
         toast({
           title: "Success",
           description: `You have saved "${
