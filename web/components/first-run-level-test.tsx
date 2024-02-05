768f49271fd5a79f139f566da5762219df6ad8be
Pawida Chumpurat <pawida.may@gmail.com>
2024-02-05 17:01:26 +0700

    fix: incorrect score calculation and add summary page

--- web/components/first-run-level-test.tsx
+++ web/components/first-run-level-test.tsx
@@ -8,7 +8,8 @@ import {
 } from "../components/ui/card";
 import { Button } from "../components/ui/button";
 import React, { useState, useEffect } from "react";
-import  ProgressBar  from "../components/progress-bar-xp";
+import ProgressBar from "../components/progress-bar-xp";
+import { toast } from './ui/use-toast'
 
 type Props = {
   userId: string;
@@ -23,54 +24,64 @@ type levelTest = {
   }[];
   points: number;
 };
+type Option = {
+  id: number;
+  text: string;
+};
 
+type Question = {
+  prompt: string;
+  options: Record<string, Option>;
+};
 export default function FirstRunLevelTest({
   userId,
   language_placement_test,
 }: Props) {
+  const [testFinished, setTestFinished] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
   const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
-  const [shuffledQuestions, setShuffledQuestions] = useState<{ prompt: string; options: Record<string, string> }[]>([]);
+  const [shuffledQuestions, setShuffledQuestions] = useState<Question[][]>([]);
   const [score, setScore] = useState(0);
-  const [isAnswered, setIsAnswered] = useState(false);
   const [correctAnswer, setCorrectAnswer] = React.useState<string[]>([]);
-  const [selectedAnswer, setSelectedAnswer] = useState(false);
+  const [rightWrongAnswer, setRightWrongAnswer] = useState(false);
   const [checked, setChecked] = useState(false);
-  const [answeredQuestions, setAnsweredQuestions] = useState<number[]>([]);
   const [formkey, setFormKey] = useState(0);
+  const [countOfRightAnswers, setCountOfRightAnswers] = useState(0);
+  const [hasAnsweredCorrectly, setHasAnsweredCorrectly] = useState(false);
+  const [isQuestionAnswered, setIsQuestionAnswered] = useState(
+    new Array(
+      language_placement_test[currentSectionIndex].questions.length
+    ).fill(false)
+  );
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [rightAnswersCounts, setRightAnswersCounts] = useState<number[]>([]);
+  const [sectionAnswerCount, setSectionAnswerCount] = useState(0);
+
+  const answerOptionIndexArray: number[] = [];
+  const answerValueArray: any[] = [];
+  const questionIndexArray: number[] = [];
 
   function getCorrectAnswer() {
-      let allCorrectAnswers: string[] = [];
+    let allCorrectAnswers: string[] = [];
     for (let i = language_placement_test.length - 1; i >= 0; i--) {
-    for (let j = language_placement_test[i].questions.length - 1; j >= 0; j--) {
+      for (
+        let j = language_placement_test[i].questions.length - 1;
+        j >= 0;
+        j--
+      ) {
         const answerA = language_placement_test[i].questions[j].options["A"];
         allCorrectAnswers.push(answerA);
+      }
+      setCorrectAnswer(allCorrectAnswers);
     }
-    setCorrectAnswer(allCorrectAnswers);
-  }}
+  }
 
-const onAnswerSelected = (answer: string, index: any) => {
-    // setAnsweredQuestions([...answeredQuestions, index]);
-    // if (!answeredQuestions.includes(index)) {
-        setIsAnswered(true);
-        try {
-            setChecked(true);
-            // setSelectedAnswerIndex(null);
-            if (correctAnswer.includes(answer)) {
-               setSelectedAnswer(true);
-               setScore(score + language_placement_test[currentSectionIndex].points);
-               console.log('true');
-           } else {
-               setSelectedAnswer(false);
-               console.log('false');
-           }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            console.log('finally');
-        }
-    // }
+  const onAnswerSelected = (optionId: number, answer: string, index: any) => {
+    answerOptionIndexArray.push(optionId);
+    answerValueArray.push(answer);
+    questionIndexArray.push(index);
   };
-  
+
   function shuffleArray(array: any[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -78,160 +89,233 @@ const onAnswerSelected = (answer: string, index: any) => {
     }
     return array;
   }
-  const handleQuestions = () => {
-      setShuffledQuestions(() => {
-                const nextQuestions = language_placement_test[
-                    currentSectionIndex
-                  ].questions.slice(0, 3);
-                  
-                      const shuffledQuestions = shuffleArray(nextQuestions);
-                      shuffledQuestions.forEach((question) => {
-                          let choices = Object.entries(question.options);
-                          for (let i = choices.length - 1; i > 0; i--) {
-                              const j = Math.floor(Math.random() * (i + 1));
-                              [choices[i], choices[j]] = [choices[j], choices[i]];
-                          }
-                          question.options = Object.fromEntries(choices);
-                      });
-                      return shuffledQuestions;
-                  });
-    
-      // if (currentSectionIndex === 0) {
-      //   // setCurrentSectionIndex(currentSectionIndex + 1);
-      //   // setFormKey(formkey + 1);
-      //   setShuffledQuestions(() => {
-      //           const nextQuestions = language_placement_test[
-      //               currentSectionIndex
-      //             ].questions.slice(0, 3);
-                  
-      //                 const shuffledQuestions = shuffleArray(nextQuestions);
-      //                 shuffledQuestions.forEach((question) => {
-      //                     let choices = Object.entries(question.options);
-      //                     for (let i = choices.length - 1; i > 0; i--) {
-      //                         const j = Math.floor(Math.random() * (i + 1));
-      //                         [choices[i], choices[j]] = [choices[j], choices[i]];
-      //                     }
-      //                     question.options = Object.fromEntries(choices);
-      //                 });
-      //                 return shuffledQuestions;
-      //             });
-      // } else if (currentSectionIndex > 0 && currentSectionIndex < language_placement_test.length - 1) {
-      //   setCurrentSectionIndex(currentSectionIndex + 1);
-      //   setFormKey(formkey + 1);
-      //   setShuffledQuestions(() => {
-      //           const nextQuestions = language_placement_test[
-      //               currentSectionIndex + 1
-      //             ].questions.slice(0, 3);
-                  
-      //                 const shuffledQuestions = shuffleArray(nextQuestions);
-      //                 shuffledQuestions.forEach((question) => {
-      //                     let choices = Object.entries(question.options);
-      //                     for (let i = choices.length - 1; i > 0; i--) {
-      //                         const j = Math.floor(Math.random() * (i + 1));
-      //                         [choices[i], choices[j]] = [choices[j], choices[i]];
-      //                     }
-      //                     question.options = Object.fromEntries(choices);
-      //                 });
-      //                 return shuffledQuestions;
-      //             });
-      // } 
 
+  function levelCalculation(score: number) {
+    const levels = [
+      { min: 0, max: 5000, cerfLevel: "A0", raLevel: "1" },
+      { min: 5001, max: 11000, cerfLevel: "A0+", raLevel: "2" },
+      { min: 11001, max: 18000, cerfLevel: "A1", raLevel: "3" },
+      { min: 18001, max: 26000, cerfLevel: "A1+", raLevel: "4" },
+      { min: 26001, max: 35000, cerfLevel: "A2-", raLevel: "5" },
+      { min: 35001, max: 45000, cerfLevel: "A2", raLevel: "6" },
+      { min: 45001, max: 56000, cerfLevel: "A2+", raLevel: "7" },
+      { min: 56001, max: 68000, cerfLevel: "B1-", raLevel: "8" },
+      { min: 68001, max: 81000, cerfLevel: "B1", raLevel: "9" },
+      { min: 81001, max: 95000, cerfLevel: "B1+", raLevel: "10" },
+      { min: 95001, max: 110000, cerfLevel: "B2-", raLevel: "11" },
+      { min: 110001, max: 126000, cerfLevel: "B2", raLevel: "12" },
+      { min: 126001, max: 143000, cerfLevel: "B2+", raLevel: "13" },
+      { min: 143001, max: 161000, cerfLevel: "C1-", raLevel: "14" },
+      { min: 161001, max: 180000, cerfLevel: "C1", raLevel: "15" },
+      { min: 180001, max: 221000, cerfLevel: "C1+", raLevel: "16" },
+      { min: 221001, max: 243000, cerfLevel: "C2-", raLevel: "17" },
+      { min: 243001, cerfLevel: "C2", raLevel: "18" }
+  ];
+
+  for (let level of levels) {
+      if (score >= level.min && (!level.max || score <= level.max)) {
+          return { cerfLevel: level.cerfLevel, raLevel: level.raLevel };
+      }
   }
+
+  return { cerfLevel: "", raLevel: "" };
+  }
+
+  const handleQuestions = () => {
+    let optionId = 0; // Initialize a counter for option IDs
+
+    let initialShuffledQuestions = [...language_placement_test]; // Copy the initial questions
+
+    let updatedShuffledQuestions = initialShuffledQuestions.map((section) => {
+      let shuffledSection = shuffleArray(section.questions).slice(0, 3); // Shuffle and slice the questions in each section
+
+      return shuffledSection.map((question) => {
+        let choices = Object.entries(question.options);
+        for (let i = choices.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [choices[i], choices[j]] = [choices[j], choices[i]];
+        }
+
+        // Assign a unique ID to each option
+        let updatedOptions = Object.fromEntries(
+          choices.map(([key, value]) => [key, { id: optionId++, text: value }])
+        );
+
+        return { ...question, options: updatedOptions }; // Return a new question object with the updated options
+      });
+    });
+
+    setShuffledQuestions(updatedShuffledQuestions); // Set the state with the new array
+  };
+
   const handleNext = () => {
-
-
-         if (currentSectionIndex < language_placement_test.length - 1) {
+    if (answerOptionIndexArray.length !== 3) {
+      toast({
+        title: "Attention!",
+        description: "Please answer all questions!",
+      });
+    } else {
+      const correctSelectedAnswer: string[] = [];
+      //For loop to check if the array answerValueArray is in the array correctAnswer
+      for (let i = 0; i < answerOptionIndexArray.length; i++) {
+        if (correctAnswer.includes(answerValueArray[i])) {
+          // console.log("Correct answer:" + answerValueArray[i]);
+          correctSelectedAnswer.push(answerValueArray[i]);
+          if (!isQuestionAnswered[questionIndexArray[i]]) {
+            setCountOfRightAnswers(countOfRightAnswers + 1);
+            const newIsQuestionAnswered = [...isQuestionAnswered];
+            newIsQuestionAnswered[questionIndexArray[i]] = true;
+            setIsQuestionAnswered(newIsQuestionAnswered);
+          }
+          setHasAnsweredCorrectly(true);
+        } else {
+          setRightWrongAnswer(false);
+          setHasAnsweredCorrectly(false);
+        }
+      }
+      setScore(
+        (prevScore) =>
+          prevScore +
+          correctSelectedAnswer.length *
+            language_placement_test[currentSectionIndex].points
+      );
+      if (correctSelectedAnswer.length >= 2) {
+        if (currentPage < shuffledQuestions.length - 1) {
+          // Save the count of correct answers for the current section
+          setRightAnswersCounts((prevCounts) => [
+            ...prevCounts,
+            correctSelectedAnswer.length,
+          ]);
+          // Proceed to the next page
+          setCurrentPage(currentPage + 1);
           setCurrentSectionIndex(currentSectionIndex + 1);
           setFormKey(formkey + 1);
-          // handleQuestions();
-    setShuffledQuestions(() => {
-        const nextQuestions = language_placement_test[
-            currentSectionIndex + 1
-          ].questions.slice(0, 3);
-          
-              const shuffledQuestions = shuffleArray(nextQuestions);
-              shuffledQuestions.forEach((question) => {
-                  let choices = Object.entries(question.options);
-                  for (let i = choices.length - 1; i > 0; i--) {
-                      const j = Math.floor(Math.random() * (i + 1));
-                      [choices[i], choices[j]] = [choices[j], choices[i]];
-                  }
-                  question.options = Object.fromEntries(choices);
-              });
-              return shuffledQuestions;
-          });
+          setChecked(false);
+          setCurrentQuestionIndex(currentQuestionIndex + 1);
+          setCountOfRightAnswers(0);
+          setSectionAnswerCount(0); // Reset sectionAnswerCount to 0 for the new section
+        } else {
+          onFinishTest();
+        }
+      } else {
+        onFinishTest();
+      }
+    }
+  };
 
-  }
-  // api update xp and level to database when click next
-    };
-    
-    useEffect(() => {
-        handleQuestions();
-        getCorrectAnswer();
+  const onFinishTest = async () => {
+    setTestFinished(true);
+    const response = await fetch(`/api/users/${userId}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        raLevel: levelCalculation(score).raLevel,
+        score: score,
+      }),
+    });
+  };
+
+  useEffect(() => {
+    handleQuestions();
+    getCorrectAnswer();
   }, []);
 
-  return (
-    <>
+  if (testFinished) {
+    return (
       <Card>
         <CardHeader>
           <CardTitle className="font-bold text-2xl md:text-2xl">
-            Let&apos;s get started by testing your skill!
+            Congratulation!
           </CardTitle>
           <CardDescription>
-            Choose the correct answer to assess your reading level.
-          </CardDescription>
+          The assessment is done.
+            </CardDescription>
         </CardHeader>
         <CardContent>
-          <div>
-            <h1 className="font-bold text-xl mb-4">
-              <span><ProgressBar progress={score} /> {score}</span><br/>
-              Section {currentSectionIndex + 1} 
-            </h1>
-            {shuffledQuestions.map(
-              (
-                question: { prompt: string; options: Record<string, string> },
-                index: number
-              ) => (
-                <div key={index}>
-                  <p className="font-bold">
-                    {index + 1}. {question.prompt}
-                  </p>
-                  <form key={formkey + 1}>
-                    {Object.entries(question.options).map(([key, value]) => (
-                      <div key={key}>
-                        <input
-                          type="radio"
-                          name={`option${index}`}
-                          value={value}
-                          id={key}
-                          className="mr-3"
-                          onChange={(e) => onAnswerSelected(e.target.value, index)}
-                          />
-                        <label htmlFor={key}>{value}</label>
-                      </div>
-                    ))}
-                  </form>
-                </div>
-              )
-            )}
-            score: {score}
-          </div>
+          <p>Your Score: {score}</p>
+          <p>Your Level: {levelCalculation(score).cerfLevel}</p><br />
+          <Button size="lg">
+            {"Get Start"}
+          </Button>
         </CardContent>
       </Card>
-      <div className="flex items-center pt-4">
-      {checked ? (
-        <Button size="lg" onClick={handleNext}>
-        {/* <Button size="lg" onClick={handleQuestions}> */}
-            {currentSectionIndex === language_placement_test.length -1 ? 'Finish' : 'Next'}
-        </Button>
-      ):(
-        <Button size="lg" onClick={handleNext} disabled>
-        {/* <Button size="lg" onClick={handleQuestions} disabled>  */}
-            {''}
-            {currentSectionIndex === language_placement_test.length -1 ? 'Finish' : 'Next'}
-        </Button>
-      )}
-      </div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <Card>
+          <CardHeader>
+            <CardTitle className="font-bold text-2xl md:text-2xl">
+              Let&apos;s get started by testing your skill!
+            </CardTitle>
+            <CardDescription>
+              Choose the correct answer to assess your reading level.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <span>
+                <ProgressBar progress={score} />
+                {score}
+              </span>
+              <br />
+              <h1 className="font-bold text-xl mb-4">
+                Section {currentSectionIndex + 1}
+              </h1>
+              {shuffledQuestions[currentPage] &&
+                shuffledQuestions[currentPage].map(
+                  (
+                    question: {
+                      prompt: string;
+                      options: Record<string, { id: number; text: string }>;
+                    },
+                    questionIndex: number
+                  ) => (
+                    <div key={questionIndex}>
+                      <p className="font-bold">
+                        {questionIndex + 1}. {question.prompt}
+                      </p>
+                      <form key={formkey + 1}>
+                        {Object.entries(question.options).map(
+                          ([key, { id, text }]) => (
+                            <div key={id}>
+                              <input
+                                type="radio"
+                                name={`option${+questionIndex}`}
+                                value={text}
+                                id={key}
+                                className="mr-3"
+                                onChange={(e) =>
+                                  onAnswerSelected(
+                                    id,
+                                    e.target.value,
+                                    questionIndex
+                                  )
+                                }
+                              />
+                              <label htmlFor={key}>{text}</label>
+                            </div>
+                          )
+                        )}
+                      </form>
+                    </div>
+                  )
+                )}
+            </div>
+          </CardContent>
+        </Card>
+        <div className="flex items-center pt-4">
+          <Button
+            size="lg"
+            onClick={
+              currentPage === shuffledQuestions.length - 1
+                ? onFinishTest
+                : handleNext
+            }
+          >
+            {currentPage === shuffledQuestions.length - 1 ? "Finish" : "Next"}
+          </Button>
+        </div>
+      </>
+    );
+  }
 }
--- web/components/progress-bar-xp.tsx
+++ web/components/progress-bar-xp.tsx
@@ -1,49 +1,7 @@
 function ProgressBar({ progress }: { progress: number }) {
-    // let maxProgress;
+    const xp = [5000, 11000, 18000, 26000, 35000, 45000, 56000, 68000, 81000, 95000, 110000, 126000, 143000, 161000, 180000, 200000, 221000, 243000];
 
-//   if (progress <= 5000) {
-//     maxProgress = 5000;
-//   } else if (progress <= 11000) {
-//     maxProgress = 11000;
-//   } else if (progress <= 18000) {
-//     maxProgress = 18000;
-//   } else if (progress <= 26000) {
-//     maxProgress = 26000;
-//     } else if (progress <= 35000) {
-//     maxProgress = 35000;
-//     } else if (progress <= 45000) {
-//     maxProgress = 45000;
-//     } else if (progress <= 56000) {
-//     maxProgress = 56000;
-//     } else if (progress <= 68000) {
-//     maxProgress = 68000;
-//     } else if (progress <= 81000) {
-//     maxProgress = 81000;
-//     } else if (progress <= 95000) {
-//     maxProgress = 95000;
-//     } else if (progress <= 110000) {
-//     maxProgress = 110000;
-//     } else if (progress <= 126000) {
-//     maxProgress = 126000;
-//     } else if (progress <= 143000) {
-//     maxProgress = 143000;
-//     } else if (progress <= 161000) {
-//     maxProgress = 161000;
-//     } else if (progress <= 180000) {
-//     maxProgress = 180000;
-//     } else if (progress <= 200000) {
-//     maxProgress = 200000;
-//     } else if (progress <= 221000) {
-//     maxProgress = 221000;
-//     } else if (progress <= 243000) {
-//     maxProgress = 243000;
-//     }   else {
-//         console.log('finished');
-        
-//     }
-    const thresholds = [5000, 11000, 18000, 26000, 35000, 45000, 56000, 68000, 81000, 95000, 110000, 126000, 143000, 161000, 180000, 200000, 221000, 243000];
-
-    let maxProgress = thresholds.find(threshold => progress <= threshold);
+    let maxProgress = xp.find(xp => progress <= xp);
     
     if (!maxProgress) {
       console.log('finished');
