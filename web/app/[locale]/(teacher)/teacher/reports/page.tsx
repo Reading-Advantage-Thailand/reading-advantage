713877e435ad0878cc0f012a8a27e122fca35775
Pawida Chumpurat <pawida.may@gmail.com>
2024-03-19 13:49:31 +0700

    feat: create mock report page

---
+++ web/app/[locale]/(teacher)/teacher/reports/page.tsx
@@ -0,0 +1,10 @@
+import Reports from "@/components/teacher/reports";
+import React from "react";
+
+export default async function rosterPage() {
+  return (
+    <div>
+      <Reports />
+    </div>
+  );
+}
