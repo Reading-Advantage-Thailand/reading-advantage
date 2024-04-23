1941e3eac605c91b2dc066dcc7da0a0e96721d04
Pawida Chumpurat <pawida.may@gmail.com>
2024-04-23 14:01:44 +0700

    refactor: refactored data mapping

--- web/components/teacher/my-students.tsx
+++ web/components/teacher/my-students.tsx
@@ -1,25 +1,36 @@
 "use client";
-import React from "react";
+import React, {useState} from "react";
 import {
   Table,
   TableBody,
   TableCell,
-  TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { table } from "console";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { CaretSortIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import { TableHead } from "@mui/material";
+import { Input } from "@/components/ui/input"
+import { useScopedI18n } from "@/locales/client"
+import Email from "next-auth/providers/email";
 
 type Student = {
   id: string;
@@ -41,12 +52,107 @@ type MyStudentProps = {
   allClassroom: Classroom[];
 };
 
-export default function MyStudents({userId, allStudent, allClassroom}: MyStudentProps) {
-console.log('all student: ', allStudent);
-console.log('all classroom :', allClassroom);
+// export const columns: ColumnDef<Student>[] = [
+//   {
+//       accessorKey: "name",
+//       header: ({ column }) => {
+//           return (
+//               <Button
+//                   variant="ghost"
+//                   onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+//               >
+//                   Name
+//                   <CaretSortIcon className="ml-2 h-4 w-4" />
+//               </Button>
+//           )
+//       },
+//       cell: ({ row }) => <div className="captoliza">{row.getValue("name")}</div>,
+//   },
+//   {
+//       accessorKey: "email",
+//       header: ({ column }) => {
+//           return (
+//               <Button
+//                   variant="ghost"
+//               >
+//                   Email
+//               </Button>
+//           )
+//       },
+//       cell: ({ row }) => <div className="captoliza">{row.getValue("email")}</div>,
+//   },
+//   {
+//     accessorKey: "action",
+//     header: () => <div className="captoliza">Action</div>,
+// },
+// ]
 
-  const tableHeader = ["Name", "Email", "Actions"];
-  const actions = ['Progress', 'Report']
+const columns: ColumnDef<{ id: string; }, any>[] = [
+  {
+      accessorKey: "name",
+      header: ({ column }) => {
+          return (
+              <Button
+                  variant="ghost"
+                  onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+              >
+                  Name
+                  <CaretSortIcon className="ml-2 h-4 w-4" />
+              </Button>
+          )
+      },
+      cell: ({ row }) => <div className="captoliza">{row.getValue("name")}</div>,
+  },
+  {
+      accessorKey: "email",
+      header: ({ column }) => {
+          return (
+              <Button
+                  variant="ghost"
+              >
+                  Email
+              </Button>
+          )
+      },
+      cell: ({ row }) => <div className="captoliza">{row.getValue("email")}</div>,
+  },
+  {
+      accessorKey: "action",
+      header: ({ column }) => {
+          return (
+              <Button
+                  variant="ghost"
+              >
+                  Action
+              </Button>
+          )
+      },
+      // cell: ({ row }) => <div className="captoliza">{row.getValue("action")}</div>,
+      cell: ({ row }) => (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="default" className="ml-auto">
+              Actions <ChevronDownIcon className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          <DropdownMenuCheckboxItem>Progress</DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem>Report</DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+        </DropdownMenu>
+      ),
+  },
+];
+
+export default function MyStudents({userId, allStudent, allClassroom}: MyStudentProps) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+    const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+        []
+    )
+    const [columnVisibility, setColumnVisibility] =
+        React.useState<VisibilityState>({})
+    const [rowSelection, setRowSelection] = React.useState({})
+    const t = useScopedI18n('components.articleRecordsTable');
 
   const studentFromAllStudent = allStudent.map((student) => {
     return student.id;
@@ -54,61 +160,121 @@ console.log('all classroom :', allClassroom);
 
   const studentFromAllClassroom = allClassroom.flatMap((classroom) => {
     return classroom.student.map((student) => {
-      console.log('student id: ', student.studentId);
-      
       return student.studentId;
     })
   });
 
   const matchedStudents = studentFromAllStudent.filter(student => studentFromAllClassroom.includes(student));
 
-  
+  const table = useReactTable({
+    data: matchedStudents.map(studentId => ({ id: studentId, name: allStudent.find(student => student.id === studentId)?.name, email: allStudent.find(student => student.id === studentId)?.email})),
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+        sorting,
+        columnFilters,
+        columnVisibility,
+        rowSelection,
+    },
+})
   return (
     <>
       <div className="font-bold text-3xl">My Students Page</div>
       {/* <CreateNewStudent userId={userId}/> */}
+      <Input
+                    placeholder={"Search..."}
+                    value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+                    onChange={(event) =>
+                        table.getColumn("name")?.setFilterValue(event.target.value)
+                    }
+                    className="max-w-sm mt-4"
+                />
       <div className="rounded-md border mt-4">
         <Table>
           <TableHeader className="font-bold">
-            <TableRow className="bg-muted/50">
-            {tableHeader.map((header, index) => (
-              <TableCell key={index}>{header}</TableCell>
-            ))}
-            </TableRow>
+          {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                  return (
+                                      <TableCell >
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                            </TableCell>
+                                    )
+                                })}
+                            </TableRow>
+                        ))}
           </TableHeader>
           <TableBody>
-            {matchedStudents.map((studentId, index) => {
-              const student = allStudent.find(student => student.id === studentId);
-              return (
-                <TableRow key={index}>
-                  <TableCell>{student?.name}</TableCell>
-                <TableCell>{student?.email}</TableCell>
-                <TableCell>
-                  <div>
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="default" className="ml-auto">
-                          Actions <ChevronDownIcon className="ml-2 h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="start">
-                        {actions.map((action, index) => {
-                          return (
-                            <DropdownMenuCheckboxItem key={index}>
-                              {action}
-                            </DropdownMenuCheckboxItem>
-                          );
-                        })}
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                  </div>
-                </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    className="cursor-pointer"
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    Empty
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
         </Table>
       </div>
+          <div className="flex items-center justify-end space-x-2 py-4">
+                <div className="flex-1 text-sm text-muted-foreground">
+                    {t('select', {
+                        selected: table.getFilteredSelectedRowModel().rows.length,
+                        total: table.getFilteredRowModel().rows.length,
+                    })}
+                </div>
+                <div className="space-x-2">
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        {t('previous')}
+                    </Button>
+                    <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        {t('next')}
+                    </Button>
+                </div>
+            </div>
     </>
   );
 }
