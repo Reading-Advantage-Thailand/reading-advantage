6f295811439234f4ae2be3647104637f4db58946
saeree <saeree_club@hotmail.co.th>
2024-04-23 22:49:31 +0700

    feat: change style tab

--- web/components/tabs.tsx
+++ web/components/tabs.tsx
@@ -1,14 +1,51 @@
 "use client";
 import React, { useState } from "react";
-import * as Tabs from "@radix-ui/react-tabs";
+// import * as Tabs from "@radix-ui/react-tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import FlashCard from "@/components/flash-card";
 import OrderSentences from "@/components/dnd/order-sentences";
 import ClozeTest from "@/components/cloze-test";
+
 
 type Props = {
   userId: string;
 };
 
+export default function TabsPractice({ userId }: Props) {
+   const [showButton, setShowButton] = useState(true);
+   return (
+     <Tabs defaultValue="tab1" className="w-full">
+       <TabsList className="grid w-full grid-cols-1 md:grid-cols-6 gap-4">
+         <TabsTrigger value="tab1">Flashcard Practice</TabsTrigger>
+         <TabsTrigger value="tab2">Order Sentences</TabsTrigger>
+         <TabsTrigger value="tab3">Cloze Test</TabsTrigger>
+         <TabsTrigger value="tab4">Order Words</TabsTrigger>
+         <TabsTrigger value="tab5">Matching</TabsTrigger>
+       </TabsList>
+       <TabsContent className="space-y-2" value="tab1">
+         <FlashCard
+           userId={userId}
+           showButton={showButton}
+           setShowButton={setShowButton}
+         />
+       </TabsContent>
+       <TabsContent className="space-y-2" value="tab2">
+         <OrderSentences userId={userId} />
+       </TabsContent>
+       <TabsContent className="space-y-2" value="tab3">
+         <ClozeTest userId={userId} />
+       </TabsContent>
+       <TabsContent className="space-y-2" value="tab4">
+         Order Words
+       </TabsContent>
+       <TabsContent className="space-y-2" value="tab5">
+         Matching
+       </TabsContent>
+     </Tabs>
+   );
+
+}
+/*
 export default function TabsPractice({ userId }: Props) {
   const [showButton, setShowButton] = useState(true);
   return (
@@ -68,3 +105,4 @@ export default function TabsPractice({ userId }: Props) {
     </Tabs.Root>
   );
 }
+*/
