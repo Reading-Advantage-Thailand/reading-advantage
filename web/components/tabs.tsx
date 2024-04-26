6c03fcf679d74a7fda4dd29db622ec1f5d605428
rtclub11140 <saeree_club@hotmail.co.th>
2024-04-26 11:28:10 +0700

    fix: edit style tabs

--- web/components/tabs.tsx
+++ web/components/tabs.tsx
@@ -14,15 +14,26 @@ type Props = {
 export default function TabsPractice({ userId }: Props) {
    const [showButton, setShowButton] = useState(true);
    return (
-     <Tabs defaultValue="tab1" className="w-full flex flex-col">
+     <Tabs
+       defaultValue="tab1"
+       className="flex flex-wrap lg:grid items-start gap-2 mb-4 lg:mb-0 overflow-auto"
+     >
        <TabsList className="flex shrink">
          <TabsTrigger value="tab1" className="w-full">
            Flashcard Practice
          </TabsTrigger>
-         <TabsTrigger value="tab2" className="w-full">Order Sentences</TabsTrigger>
-         <TabsTrigger value="tab3" className="w-full">Cloze Test</TabsTrigger>
-         <TabsTrigger value="tab4" className="w-full">Order Words</TabsTrigger>
-         <TabsTrigger value="tab5" className="w-full">Matching</TabsTrigger>
+         <TabsTrigger value="tab2" className="w-full">
+           Order Sentences
+         </TabsTrigger>
+         <TabsTrigger value="tab3" className="w-full">
+           Cloze Test
+         </TabsTrigger>
+         <TabsTrigger value="tab4" className="w-full">
+           Order Words
+         </TabsTrigger>
+         <TabsTrigger value="tab5" className="w-full">
+           Matching
+         </TabsTrigger>
        </TabsList>
        <TabsContent className="space-y-2" value="tab1">
          <FlashCard
