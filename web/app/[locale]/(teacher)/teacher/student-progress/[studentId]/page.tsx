74ce657b16652fdd9112df7fb39bdef6134797aa
Pawida Chumpurat <pawida.may@gmail.com>
2024-04-28 10:48:48 +0700

    fix: change student level on progress page in  my student page.

--- web/app/[locale]/(teacher)/teacher/student-progress/[studentId]/page.tsx
+++ web/app/[locale]/(teacher)/teacher/student-progress/[studentId]/page.tsx
@@ -11,6 +11,7 @@ import {
 import { getCurrentUser } from "@/lib/session";
 import { headers } from "next/headers";
 import React from "react";
+import ResetDialog from "@/components/reset-xp-dialog";
 
 async function getStudentProgress(studentId: string) {
     const res = await fetch (
@@ -29,38 +30,47 @@ export default async function ProgressPage({params}: {params: {studentId: string
   const resStudentProgress = await getStudentProgress(params.studentId);
   const userId = user?.id ?? '';  
   let nameOfStudent = '';
-  
+  let studentLevel: number = 0;
+
   if (userId === resStudentProgress.articles[0]?.userId) {
     nameOfStudent = user?.name?? 'Unknown';
+    studentLevel = user?.level ?? 0;
   }
 
   return (
     <>
-      <Header heading={`Progress of ${nameOfStudent}`} />
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7 mt-4 mb-10">
-        <Card className="md:col-span-4">
-          <CardHeader>
-            <CardTitle>Activity</CardTitle>
-          </CardHeader>
-          <CardContent className="pl-2">
-            <UserActivityChart data={resStudentProgress.articles} />
-          </CardContent>
-        </Card>
-        <Card className="md:col-span-3">
-          <CardHeader>
-            <CardTitle>Level</CardTitle>
-            <CardDescription>
-              Your current level is {resStudentProgress.articles[0]?.userLevel || 0}
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <UserLevelChart
-              data={resStudentProgress.articles}
-              resGeneralDescription={resStudentProgress}
-            />
-          </CardContent>
-        </Card>
-      </div>
+{resStudentProgress.articles[0]?.userId ? (
+  <div>
+<Header heading={`Progress of ${nameOfStudent}`} />
+  <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7 mt-4 mb-10">
+    <Card className="md:col-span-4">
+      <CardHeader>
+        <CardTitle>Activity</CardTitle>
+      </CardHeader>
+      <CardContent className="pl-2">
+        <UserActivityChart data={resStudentProgress.articles} />
+      </CardContent>
+    </Card>
+    <Card className="md:col-span-3">
+      <CardHeader>
+        <CardTitle>Level</CardTitle>
+        <CardDescription>
+          Your current level is {studentLevel}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <UserLevelChart
+          data={resStudentProgress.articles}
+          resGeneralDescription={resStudentProgress}
+        />
+      </CardContent>
+    </Card>
+  </div>
+  {/* <ResetDialog /> */}
+  </div>
+) : (
+  <p className="text-center">No user progress available.</p>
+)}
     </>
   );
 }
