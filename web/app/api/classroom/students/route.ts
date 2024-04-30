b9bc7f1657c41ec7365482c79a695980a65c8eff
Pawida Chumpurat <pawida.may@gmail.com>
2024-04-30 16:34:20 +0700

    refactor: remove unuses code and page

--- web/app/api/classroom/students/route.ts
+++ web/app/api/classroom/students/route.ts
@@ -2,49 +2,6 @@ import db from "@/configs/firestore-config";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
-// get user info
-// export async function GET(req: Request, res: Response) {
-//     try {
-//       const session = await getServerSession(authOptions);
-//       if (!session) {
-//         return new Response(
-//           JSON.stringify({
-//             message: "Unauthorized",
-//           }),
-//           { status: 403 }
-//         );
-//       }
-  
-//       const userId = session.user.id;
-  
-//       const user = await db.collection("users").doc(userId).get();
-  
-//       if (!user.exists) {
-//         return new Response(
-//           JSON.stringify({
-//             message: "User not found",
-//           }),
-//           { status: 404 }
-//         );
-//       }
-  
-//       return new Response(
-//         JSON.stringify({
-//           message: "success",
-//           data: user.data(),
-//         }),
-//         { status: 200 }
-//       );
-//     } catch (error) {
-//       return new Response(
-//         JSON.stringify({
-//           message: error,
-//         }),
-//         { status: 500 }
-//       );
-//     }
-//   }
-
   export async function GET(req: Request, res: Response) {
     try {
         const session = await getServerSession(authOptions);
