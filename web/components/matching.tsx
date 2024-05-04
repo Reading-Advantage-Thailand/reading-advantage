dec91d31e204360a4c5f3272d36cd849fa80aaff
rtclub11140 <saeree_club@hotmail.co.th>
2024-05-04 14:42:23 +0700

    feat: initial tab matching

---
+++ web/components/matching.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// "use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import dayjs_plugin_isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import dayjs_plugin_isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import { useScopedI18n } from "@/locales/client";
+import Image from "next/image";
+import styled from "@emotion/styled";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { Header } from "./header";
+import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
+import { Skeleton } from "./ui/skeleton";
+import { updateScore } from "@/lib/utils";
+import { Sentence } from "./dnd/types";
+import AudioButton from "./audio-button";
+dayjs.extend(utc);
+dayjs.extend(dayjs_plugin_isSameOrBefore);
+dayjs.extend(dayjs_plugin_isSameOrAfter);
+
+type Props = {
+  userId: string;
+};
+
+export default function Matching({ userId }: Props) {
+  const t = useScopedI18n("pages.student.practicePage");
+  const tUpdateScore = useScopedI18n(
+    "pages.student.practicePage.flashcardPractice"
+  );
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [currentArticleIndex, setCurrentArticleIndex] = useState(0);
+  const [showBadges, setShowBadges] = useState(false);
+  const [showButtonNextPassage, setShowButtonNextPassage] = useState(false);
+  const [selectedAnswers, setSelectedAnswers] = useState<number[]>([]);
+
+  useEffect(() => {
+    getUserSentenceSaved();
+  }, []);
+
+  const getUserSentenceSaved = async () => {}
+
+  return <></>;
+}
