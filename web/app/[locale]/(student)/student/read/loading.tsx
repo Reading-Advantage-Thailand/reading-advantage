d01d37a4e72265890fee3119b2934b6ee538dbfa
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2024-05-05 17:11:03 +0700

    update: article selector (moved to using search params)

--- web/app/[locale]/(student)/student/read/loading.tsx
+++ web/app/[locale]/(student)/student/read/loading.tsx
@@ -1,18 +1,36 @@
-import { Skeleton } from "@/components/ui/skeleton"
+import { Header } from "@/components/header";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { getScopedI18n } from "@/locales/server";
 
-export default function Loading() {
-    return (
-        <div className="grid w-full gap-10">
-            <div className="flex w-full items-center justify-between">
-                <Skeleton className="h-[38px] w-[90px]" />
-                <Skeleton className="h-[38px] w-[80px]" />
-            </div>
-            <div className="mx-auto w-[800px] space-y-6">
-                <Skeleton className="h-[50px] w-full" />
-                <Skeleton className="h-[20px] w-2/3" />
-                <Skeleton className="h-[20px] w-full" />
-                <Skeleton className="h-[20px] w-full" />
-            </div>
-        </div>
-    )
+export default async function Loading() {
+  const t = await getScopedI18n("pages.student.readPage");
+
+  return (
+    <>
+      <Header heading={t("articleSelection")} />
+      <Card className="my-2">
+        <CardHeader>
+          <CardTitle>
+            <Skeleton className="h-[18px] w-2/3" />
+          </CardTitle>
+          <CardDescription>
+            <Skeleton className="h-[14px] w-1/3" />
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid sm:grid-cols-2 grid-flow-row gap-4">
+            <Skeleton className="h-[20rem] w-full" />
+            <Skeleton className="h-[20rem] w-full" />
+          </div>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
---
+++ web/app/api/v1/articles/route.ts
@@ -0,0 +1,32 @@
+import { getSearchArticles } from "@/backend/controllers/article_controller";
+import { logRequest } from "@/backend/utils/middleware";
+import { createEdgeRouter } from "next-connect";
+import { NextResponse, type NextRequest } from "next/server";
+
+
+const router = createEdgeRouter<NextRequest, { params?: unknown }>();
+
+router.use(logRequest);
+
+// Middleware
+// router.use(async (
+//     req: NextRequest,
+//     params: unknown,
+//     next: () => void
+// ) => {
+
+
+//     // Check level is be the same as the user's level
+//     const userLevel = session.user.level;
+//     const level = new Number(req.nextUrl.searchParams.get('level')).valueOf();
+//     if (level !== userLevel) {
+//         req.nextUrl.searchParams.set('level', userLevel.toString());
+//     }
+//     return next();
+// });
+
+router.get(getSearchArticles);
+
+export async function GET(request: NextRequest, ctx: { params?: unknown }) {
+    return router.run(request, ctx);
+}
