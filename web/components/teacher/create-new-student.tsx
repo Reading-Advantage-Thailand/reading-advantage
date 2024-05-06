1c96aa07415c16d31ce462f9eb2e3b1913d58f81
Pawida Chumpurat <pawida.may@gmail.com>
2024-05-06 21:54:10 +0700

    update: add style and fix related field

--- web/components/teacher/create-new-student.tsx
+++ web/components/teacher/create-new-student.tsx
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import {
   Card,
-  CardHeader,
   CardContent,
   CardDescription,
   CardFooter,
@@ -10,17 +9,20 @@ import {
 } from "@/components/ui/card";
 import * as Tabs from "@radix-ui/react-tabs";
 import Link from "next/link";
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Table, TableHeader, TableCell, TableBody, TableRow, TableHead } from "@/components/ui/table";
-// import { TableBody,  TableHead, TableRow } from "@mui/material";
+import {
+  Table,
+  TableHeader,
+  TableCell,
+  TableBody,
+  TableRow,
+} from "@/components/ui/table";
 
 function CreateNewStudent() {
-  const [open, setOpen] = useState(false);
   const classroomName = "Math 101";
   const classCode = "ABC123";
+  const [inputs, setInputs] = useState(0);
 
   return (
-
     <div>
       <Card className="flex flex-col items-center justify-center">
         <CardTitle className="mt-10 mb-4 text-3xl text-[#3882fd]">
@@ -29,27 +31,38 @@ function CreateNewStudent() {
         <CardDescription className="text-base mb-4">
           There are two ways to add students to the classroom
         </CardDescription>
-        <Tabs.Root className="mx-[10%]">
-          <Tabs.List className="h-fit grid grid-cols-2 md:grid-cols-2 bg-[#3882fd]">
-            <Tabs.Trigger className="mr-2" value="class code">
-              Class Code
-            </Tabs.Trigger>
-            <Tabs.Trigger value="manually">Manually</Tabs.Trigger>
-          </Tabs.List>
+        <Tabs.Root>
+          <div className="flex justify-center items-center">
+            <Tabs.List className="h-fit w-fit grid grid-cols-2 md:grid-cols-2 rounded-full border-4 overflow-x-auto border-[#314265]">
+              <Tabs.Trigger
+                className="focus:text-white focus:bg-[#314265] rounded-l-full p-2"
+                value="class code"
+              >
+                Send Code
+              </Tabs.Trigger>
+              <Tabs.Trigger
+                value="manually"
+                className=" focus:text-white focus:bg-[#314265]  rounded-r-full p-2"
+              >
+                Add Manually
+              </Tabs.Trigger>
+            </Tabs.List>
+          </div>
+          {/* class code */}
           <Tabs.Content
             value="class code"
-            className="flex flex-col items-center w-full"
+            className="flex flex-col items-center"
           >
             <Card className="flex flex-col items-center justify-center my-4 ">
-              <div className="space-y-2 flex flex-col items-center overflow-auto mt-8 w-[67%]">
-                <CardContent className="text-base overflow-auto text-center">
-                  Share this class code with your students.
-                  <br />
-                  They can enter the code when they sign up and they will
-                  automatically join the class you have just created.
-                </CardContent>
-              </div>
-              <CardContent className="flex flex-col w-[50%] mt-6">
+              {/* <div className="flex flex-col items-center overflow-auto mt-8 "> */}
+              <CardContent className="text-base overflow-auto text-center w-[67%] mt-8">
+                Share this class code with your students.
+                <br />
+                They can enter the code when they sign up and they will
+                automatically join the class you have just created.
+              </CardContent>
+              {/* </div> */}
+              <CardContent className="flex flex-col mt-6">
                 Class Code:{" "}
                 <input
                   type="text"
@@ -62,84 +75,149 @@ function CreateNewStudent() {
                 <Link href="">Download handout</Link>
               </CardFooter>
             </Card>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-full flex justify-center">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-full border-2 border-[#314265]">
               Done
             </button>
           </Tabs.Content>
-   
-           {/* manually */}
-            <CardDescription className="flex justify-end mr-[10%]">
+
+          {/* manually */}
+          <Tabs.Content
+            value="manually"
+            className="flex flex-col items-center mb-8"
+          >
+            <div className="flex ml-auto mr-[10%] cursor-pointer text-[#3882fd]">
               Import from CSV
-            </CardDescription>
-          <Tabs.Content value="manually" className="flex flex-col items-center mb-8 ">
-            <Card className="flex flex-col items-center justify-center my-4 w-[90%]">
-              <Table>
-                <TableHeader>
-                  <TableRow className="h-[50px]">
-                    <TableCell className="p-4 ">First Name</TableCell>
-                    <TableCell className="p-4 ">Last Name</TableCell>
-                    <TableCell className="p-4 ">Email</TableCell>
-                    <TableCell className="p-4 ">User Name</TableCell>
-                    <TableCell className="p-4 ">Password</TableCell>
-                  </TableRow>
-                </TableHeader>
-                <TableBody >
-                  <TableCell>
-                    <input
-                      type="text"
-                      placeholder="First Name"
-                      className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent "
-                    />
-                  </TableCell>
-                <TableCell >
-                    <input
+            </div>
+              <Card className="my-4">
+                <Table>
+                  <TableHeader>
+                    <TableRow className="h-[50px]">
+                      <TableCell className="p-4">First Name</TableCell>
+                      <TableCell className="p-4">Last Name</TableCell>
+                      <TableCell className="p-4">Email</TableCell>
+                      <TableCell className="p-4">User Name</TableCell>
+                      <TableCell className="p-4">Password</TableCell>
+                    </TableRow>
+                  </TableHeader>
+                  {Array.from({ length: inputs }).map(
+                    (_: any, index: number) => (
+                      <TableBody key={index}>
+                        <TableCell>
+                          <input
+                            type="text"
+                            placeholder="First Name"
+                            className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent "
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <input
+                            type="text"
+                            placeholder="Last Name"
+                            className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
+                            onInvalid={(
+                              e: React.ChangeEvent<HTMLInputElement>
+                            ) => {
+                              e.target.setCustomValidity(
+                                "Last Name is required"
+                              );
+                            }}
+                            onInput={(
+                              e: React.ChangeEvent<HTMLInputElement>
+                            ) => {
+                              e.target.setCustomValidity("");
+                            }}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <input
+                            type="text"
+                            placeholder="Email(Optional)"
+                            className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <input
+                            type="text"
+                            placeholder="User Name"
+                            className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <input
+                            type="text"
+                            placeholder="Password"
+                            className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
+                          />
+                        </TableCell>
+                      </TableBody>
+                    )
+                  )}
+                  <TableBody>
+                    <TableCell>
+                      <input
+                        type="text"
+                        placeholder="First Name"
+                        className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent overflow-x-auto"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <input
                         type="text"
                         placeholder="Last Name"
-                        className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
+                        className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent overflow-x-auto"
                         onInvalid={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            e.target.setCustomValidity("Last Name is required");
+                          e.target.setCustomValidity("Last Name is required");
                         }}
                         onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            e.target.setCustomValidity("");
+                          e.target.setCustomValidity("");
                         }}
-                    />
-                </TableCell>
-                  <TableCell>
-                    <input
-                      type="text"
-                      placeholder="Email(Optional)"
-                      className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="text"
-                      placeholder="User Name"
-                      className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <input
-                      type="text"
-                      placeholder="Password"
-                      className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent"
                       />
-                  </TableCell>
-                </TableBody>
-              </Table>
-                      <CardDescription className="flex justify-end w-full m-4 p-4 mr-8 text-[#3882fd] cursor-pointer">Add new student</CardDescription>
-                      <CardDescription className="text-center w-full m-4 p-4 mr-8 text-red-500 mb-16">To add a student, please fill in the required fields above.
-</CardDescription>
-            </Card>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-full mt-2">
+                    </TableCell>
+                    <TableCell>
+                      <input
+                        type="text"
+                        placeholder="Email(Optional)"
+                        className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent overflow-x-auto"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <input
+                        type="text"
+                        placeholder="User Name"
+                        className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent overflow-x-auto"
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <input
+                        type="text"
+                        placeholder="Password"
+                        className="hover:border-none border-b p-2 focus:outline-none focus:border-transparent overflow-x-auto"
+                      />
+                    </TableCell>
+                  </TableBody>
+                </Table>
+                <Link
+                  href=""
+                  className="flex justify-end m-4 p-4 text-[#3882fd] cursor-pointer"
+                  onClick={() => setInputs((prevInputs) => prevInputs + 1)}
+                >
+                  Add new student
+                </Link>
+                <CardDescription className="text-center w-full m-4 p-4 mr-8 text-red-500 mb-16">
+                  To add a student, please fill in the required fields above.
+                </CardDescription>
+              </Card>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-full mt-2 flex border-2 border-[#314265]">
               SAVE AND CONTINUE
             </button>
-            <CardDescription className="space-y-2 text-center overflow-auto mt-8 w-[67%]">
-            * Clicking the save button will create these student accounts and you will receive an email with a file containing these credentials for future use. If you do not see the email in a few minutes, check your 'junk mail', 'spam' or 'promotions' folder.
+            <CardDescription className="text-center overflow-auto mt-8 md:w-[85%] lg:w-50%]">
+              * Clicking the save button will create these student accounts and
+              you will receive an email with a file containing these credentials
+              for future use. If you do not see the email in a few minutes,
+              check your 'junk mail', 'spam' or 'promotions' folder.
             </CardDescription>
           </Tabs.Content>
         </Tabs.Root>
-        {/* <CardContent></CardContent> */}
       </Card>
     </div>
   );
