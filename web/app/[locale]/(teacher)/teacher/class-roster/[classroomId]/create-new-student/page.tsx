3f4fbde80de69d987cc9e15d49de789d6dc3f741
Pawida Chumpurat <pawida.may@gmail.com>
2024-05-06 10:21:41 +0700

    feat: create AddNewStudent page

---
+++ web/app/[locale]/(teacher)/teacher/class-roster/[classroomId]/create-new-student/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateNewStudent from '@/components/teacher/create-new-student';
+
+
+export default function AddNewStudent() {
+  
+  return (
+    <>
+  <CreateNewStudent/>
+    </>
+  )
+}
