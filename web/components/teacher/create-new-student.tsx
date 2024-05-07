c0eeeeb93e26daba5074b3c057a2bb67a4edb45e
Pawida Chumpurat <pawida.may@gmail.com>
2024-05-07 09:42:44 +0700

    refactor: add icons and adjust the page to just manually page only

--- web/components/teacher/create-new-student.tsx
+++ web/components/teacher/create-new-student.tsx
@@ -16,13 +16,17 @@ import {
   TableBody,
   TableRow,
 } from "@/components/ui/table";
+import { Icons } from "@/components/icons";
 
 function CreateNewStudent() {
   const classroomName = "Math 101";
   const classCode = "ABC123";
   const [inputs, setInputs] = useState(0);
 
-
+const handleAddStudent = () => {
+    console.log("Add student");
+    
+}
   return (
     <div>
       <Card className="flex flex-col items-center justify-center">
@@ -32,8 +36,8 @@ function CreateNewStudent() {
         <CardDescription className="text-base mb-4">
           There are two ways to add students to the classroom
         </CardDescription>
-        <Tabs.Root>
-          <div className="flex justify-center items-center">
+        {/* <Tabs.Root> */}
+          {/* <div className="flex justify-center items-center">
             <Tabs.List className="h-fit w-fit grid grid-cols-2 md:grid-cols-2 rounded-full border-4 overflow-x-auto border-[#314265]">
               <Tabs.Trigger
                 className="focus:text-white focus:bg-[#314265] rounded-l-full p-2"
@@ -48,21 +52,19 @@ function CreateNewStudent() {
                 Add Manually
               </Tabs.Trigger>
             </Tabs.List>
-          </div>
+          </div> */}
           {/* class code */}
-          <Tabs.Content
+          {/* <Tabs.Content
             value="class code"
             className="flex flex-col items-center"
           >
             <Card className="flex flex-col items-center justify-center my-4 ">
-              {/* <div className="flex flex-col items-center overflow-auto mt-8 "> */}
               <CardContent className="text-base overflow-auto text-center w-[67%] mt-8">
                 Share this class code with your students.
                 <br />
                 They can enter the code when they sign up and they will
                 automatically join the class you have just created.
               </CardContent>
-              {/* </div> */}
               <CardContent className="flex flex-col mt-6">
                 Class Code:{" "}
                 <input
@@ -79,16 +81,16 @@ function CreateNewStudent() {
             <button className="bg-blue-500 text-white px-4 py-2 rounded-full border-2 border-[#314265]">
               Done
             </button>
-          </Tabs.Content>
+          </Tabs.Content> */}
 
           {/* manually */}
-          <Tabs.Content
-            value="manually"
+          <CardContent
+            // value="manually"
             className="flex flex-col items-center mb-8"
           >
-            <div className="flex ml-auto mr-[10%] cursor-pointer text-[#3882fd]">
+            {/* <div className="flex ml-auto mr-[10%] cursor-pointer text-[#3882fd]">
               Import from CSV
-            </div>
+            </div> */}
               <Card className="my-4">
                 <Table>
                   <TableHeader>
@@ -202,13 +204,13 @@ function CreateNewStudent() {
                   className="flex justify-end m-4 p-4 text-[#3882fd] cursor-pointer"
                   onClick={() => setInputs((prevInputs) => prevInputs + 1)}
                 >
-                  Add new student
+                  Add new student <Icons.addUser className="w-5 ml-2" />
                 </Link>
                 <CardDescription className="text-center w-full m-4 p-4 mr-8 text-red-500 mb-16">
                   To add a student, please fill in the required fields above.
                 </CardDescription>
               </Card>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-full mt-2 flex border-2 border-[#314265]">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-full mt-2 flex border-2 border-[#314265]" onClick={handleAddStudent}>
               SAVE AND CONTINUE
             </button>
             <CardDescription className="text-center overflow-auto mt-8 md:w-[85%] lg:w-50%]">
@@ -217,8 +219,8 @@ function CreateNewStudent() {
               for future use. If you do not see the email in a few minutes,
               check your 'junk mail', 'spam' or 'promotions' folder.
             </CardDescription>
-          </Tabs.Content>
-        </Tabs.Root>
+          </CardContent>
+        {/* </Tabs.Root> */}
       </Card>
     </div>
   );
