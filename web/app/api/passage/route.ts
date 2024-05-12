7db70596155d4886e6cf99739eddd0fc78d8c12f
Pawida Chumpurat <pawida.may@gmail.com>
2024-05-12 16:01:07 +0700

    update: api passage route to get all article

--- web/app/api/passage/route.ts
+++ web/app/api/passage/route.ts
@@ -3,84 +3,36 @@ import db from "@/configs/firestore-config";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
-// GET /api/v1/articles?level=10&type=fiction&genre=Fantasy
-export async function getSearchArticles(req: NextRequest) {
+
+export async function GET(req: Request, res: Response) {
     try {
         const session = await getServerSession(authOptions);
         if (!session) {
-            return NextResponse.json({ message: "Unauthorized" }, { status: 403 });
+        return new Response(
+            JSON.stringify({
+            message: "Unauthorized",
+            }),
+            { status: 403 }
+        );
         }
-
-        // if (session.user.role.includes("TEACHER")) {
-        //     const articles = await db.collection("articles_new").get();
-        //     console.log('articles', articles.docs.map(doc => doc.data()));
-        //        return NextResponse.json({
-        //            articles: articles.docs.map(doc => doc.data()),
-        //        });
-        //    }
         
- 
-        const level = session.user.level.toString();
-        const type = req.nextUrl.searchParams.get("type");
-        const genre = req.nextUrl.searchParams.get("genre");
-        const subgenre = req.nextUrl.searchParams.get("subgenre");
-
-        if (!level) {
-            return NextResponse.json(
-                { message: "Level is required" },
-                { status: 400 }
-            );
-        }
+        const userId = session.user.id;
+        const articlesRef = db.collection('articles_new');
+        const snapshot = await articlesRef.get();
+        const articles = snapshot.docs.map(doc => doc.data());
     
-        let data = db.collection("levels-test").doc(level);
-    
-        if (type) {
-            data = data.collection("types").doc(type);
-        }
-        if (genre) {
-            data = data.collection("genres").doc(genre);
-        }
-    
-        let results: any[] = [];
-        if (subgenre) {
-            const snapshot = await data
-                .collection("subgenres")
-                .doc(subgenre)
-                .collection("articles")
-                .get();
-            snapshot.forEach((doc) => {
-                results.push(doc.data());
-            });
-    
-            // Check if user has read the article
-            for (let i = 0; i < results.length; i++) {
-                const articleRecord = await db
-                    .collection("user-article-records")
-                    .doc(`${session.user.id}-${results[i].id}`)
-                    .get();
-                results[i].is_read = articleRecord.exists;
-            }
-        } else {
-            const snapshot = await data.get();
-            results = Object.entries(snapshot.data() as any).map(
-                ([key, value]) => key
-            );
-        }
-    
-        return NextResponse.json({
-            params: {
-                level,
-                type,
-                genre,
-                subgenre,
-            },
-            results,
-        });
-    } catch (err) {
-        console.log("Error getting documents", err);
-        return NextResponse.json(
-            { message: `Error getting documents: ${err}`, results: [] },
-            { status: 500 }
+        return new Response(
+        JSON.stringify({
+            passages: articles,
+        }),
+        { status: 200 }
+        );
+    } catch (error) {
+        return new Response(
+        JSON.stringify({
+            message: error,
+        }),
+        { status: 500 }
         );
     }
-}
+    }
