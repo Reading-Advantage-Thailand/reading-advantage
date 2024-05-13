4ae8a91f8d10bb19b4746a31f0d698197090468e
Pawida Chumpurat <pawida.may@gmail.com>
2024-05-13 10:20:29 +0700

    create assign dialog
    Not yet complete, still remaining student are displayed in the class list

--- web/components/teacher/assign-dialog.tsx
+++ web/components/teacher/assign-dialog.tsx
@@ -1,52 +1,70 @@
-'use client';
-import React, {useState} from 'react'
-import { Button } from '../ui/button'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '../ui/dialog';
-import { ArticleType } from "@/types";
+"use client";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { Article } from "@/components/models/article-model";
 import {
-    DropdownMenu,
-    DropdownMenuCheckboxItem,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu";
-
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 type Props = {
-    article: ArticleType;
-    articleId: string;
-    userId: string;
-}
+  article: Article;
+  articleId: string;
+  userId: string;
+};
+const mockClassrooms = [
+  {
+    id: "1",
+    name: "Class 1",
+  },
+  {
+    id: "2",
+    name: "Class 2",
+  },
+  {
+    id: "3",
+    name: "Class 3",
+  },
+];
 
-export default function AssignDialog({article, articleId, userId}: Props) {
-      const [show, setShow] = useState(false);
+export default function AssignDialog({ article, articleId, userId }: Props) {
+  const [show, setShow] = useState(false);
 
-    const handleShow = () => {
-        setShow(true);
-    };
+  const handleShow = () => {
+    setShow(true);
+  };
 
-    const handleClose = () => {
-        setShow(false);
-    };
+  const handleClose = () => {
+    setShow(false);
+  };
   return (
-    <div>   
-    <Button className="mt-4" onClick={handleShow}>Assign</Button>
-        <Dialog open={show} onOpenChange={handleClose}>
-                    <DialogContent className='text-center'>
-                    <DialogTitle>Assign :</DialogTitle>
-                    <DialogTitle>{article.title}</DialogTitle>
-                    <p>Class</p>
-                   <DropdownMenu>
-                        <DropdownMenuTrigger>
-                            <Button>Choose Class</Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent>
-                            <DropdownMenuCheckboxItem>Class 1</DropdownMenuCheckboxItem>
-                            <DropdownMenuCheckboxItem>Class 2</DropdownMenuCheckboxItem>
-                            <DropdownMenuCheckboxItem>Class 3</DropdownMenuCheckboxItem>
-                        </DropdownMenuContent>
-                   </DropdownMenu>
-                    </DialogContent>
-              </Dialog>
+    <div>
+      <Button className="mb-4 ml-5" onClick={handleShow}>
+        Assign
+      </Button>
+      <Dialog open={show} onOpenChange={handleClose}>
+        <DialogContent className="text-center">
+          <DialogTitle>Assign :</DialogTitle>
+          <DialogTitle>{article.title}</DialogTitle>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+            Choose Class: 
+              <Button variant="outline" className="ml-2">
+            <select>
+              {mockClassrooms.map((classroom) => (
+                <option key={classroom.id} value={classroom.id}>
+                  {classroom.name}
+                </option>
+              ))}
+            </select>
+              </Button>
+            </DropdownMenuTrigger>
+          </DropdownMenu>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
