d80111dc3932ef8456e46137b46320476541e754
Pawida Chumpurat <pawida.may@gmail.com>
2024-05-14 17:46:49 +0700

    Test case for my-students component

--- web/__tests__/my-students.test.tsx
+++ web/__tests__/my-students.test.tsx
@@ -1,51 +1,224 @@
-import React, { useState, useEffect } from 'react';
-import { render, fireEvent, waitFor, getByText } from '@testing-library/react';
-import MyStudents from '../components/teacher/my-students';
-import { I18nProviderClient } from '../locales/client';
-import { screen } from "@testing-library/dom";
-import { useI18n } from '../locales/client';
+import React from "react";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor
+} from "@testing-library/react";
+import MyStudents from "../components/teacher/my-students";
+import { I18nProviderClient, useScopedI18n } from "../locales/client";
+import { useRouter } from "next/navigation";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
-jest.mock('../locales/client', () => ({
-    ...jest.requireActual('react'),
-    useI18n: () => ({
-      t: (key: any) => key, 
-    }),
-  }));
+useScopedI18n("components.articleRecordsTable");
+jest.mock("../locales/client", () => ({
+  useScopedI18n: () => (key: string) => key,
+}));
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}));
+jest.mock('next/router', () => ({
+  userRouter: jest.fn()
+}));
+jest.mock("axios");
 
-describe('MyStudents page', () => {
-//   test('can render the page', () => {
-//     render(
-//         <I18nProviderClient locale="en">
-//           <MyStudents userId="" matchedStudents={[]} />
-//         </I18nProviderClient>
-//       );
-//       expect(screen.getByText("My Classroom")).toBeInTheDocument();
-//   });
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
-test('can search', async () => {
-    const { getByPlaceholderText, getByText } = render(
-      <I18nProviderClient locale="en">
-        <MyStudents userId={''} matchedStudents={[]} />
-      </I18nProviderClient>
-    );
-    const searchInput = getByPlaceholderText('Search'); 
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
-    fireEvent.change(searchInput, { target: { value: 'John' } }); 
 
-    await waitFor(() => {
-        expect(getByText('John')).toBeInTheDocument(); 
-    });
+
+describe("MyStudents page", () => {
+  it("should render student page", async () => {
+    const matchedStudents = [
+      { id: "1", email: "may@gmail.com", name: "may" },
+      { id: "2", email: "adam@gmail.com", name: "Adam" },
+      { id: "3", email: "test3@test.com", name: "Test Student 3" },
+      { id: "4", email: "test4@test.com", name: "Test Student 4" },
+      { id: "5", email: "test5@test.com", name: "Test Student 5" },
+    ];
+    render(<MyStudents matchedStudents={matchedStudents} />);
+
+    const myStudentsPageElement = screen.getByText("My Students Page");
+    expect(myStudentsPageElement).toBeInTheDocument();
   });
 
-//   test('can sort by name', async () => {
-//     const { getByText } = render(<MyStudents userId={''} matchedStudents={[]} />);
-//     const sortButton = getByText('Name'); // adjust this to your sort button's text
+  it("should render student page with a list of students", async () => {
+    const matchedStudents = [
+      { id: "1", email: "may@gmail.com", name: "may" },
+      { id: "2", email: "adam@gmail.com", name: "Adam" },
+      { id: "3", email: "test3@test.com", name: "Test Student 3" },
+      { id: "4", email: "test4@test.com", name: "Test Student 4" },
+      { id: "5", email: "test5@test.com", name: "Test Student 5" },
+    ];
+    render(<MyStudents matchedStudents={matchedStudents} />);
 
-//     fireEvent.click(sortButton); // simulate clicking the sort button
+    const myStudentsNameElement1 = screen.getByText("may");
+    expect(myStudentsNameElement1).toBeInTheDocument();
+    const myStudentsNameElement2 = screen.getByText("Adam");
+    expect(myStudentsNameElement2).toBeInTheDocument();
+    const myStudentsNameElement3 = screen.getByText("Test Student 3");
+    expect(myStudentsNameElement3).toBeInTheDocument();
+    const myStudentsNameElement4 = screen.getByText("Test Student 4");
+    expect(myStudentsNameElement4).toBeInTheDocument();
+    const myStudentsNameElement5 = screen.getByText("Test Student 5");
+    expect(myStudentsNameElement5).toBeInTheDocument();
+  });
 
-//     await waitFor(() => {
-//       // adjust this to check that the first student's name in the sorted list is displayed
-//       expect(getByText('Adam')).toBeInTheDocument(); // assuming 'Adam' would be the first name when sorted
-//     });
-//   });
+  it("should render all available actions in dropdown list", async () => {
+    const matchedStudents = [
+      { id: "1", email: "test1@test.com", name: "Test Student 1" },
+    ];
+
+    render(<MyStudents matchedStudents={matchedStudents} />);
+
+    const myStudentsNameElement1 = screen.getByText(matchedStudents[0].name);
+    expect(myStudentsNameElement1).toBeInTheDocument();
+    const actionsButton = screen.getByText("Actions");
+    userEvent.click(actionsButton);
+    const progressButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Progress",
+    });
+    expect(progressButton).toBeInTheDocument();
+    const enrollButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Enroll",
+    });
+    expect(enrollButton).toBeInTheDocument();
+    const UnenrollButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Unenroll",
+    });
+    expect(UnenrollButton).toBeInTheDocument();
+    const resetProgressButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Reset Progress",
+    });
+    expect(resetProgressButton).toBeInTheDocument();
+  });
+
+  it("should redirect to progress page", async () => {
+    const matchedStudents_progress = [
+      { id: "103186496931574456567", email: "rtclub11140@gmail.com", name: "Saeree t." },
+    ];
+
+    const push = jest.fn();
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      push
+    }));
+
+
+    render(
+      <MyStudents matchedStudents={matchedStudents_progress} />
+    );
+
+    const myStudentsNameElement1 = screen.getByText(matchedStudents_progress[0].name);
+    expect(myStudentsNameElement1).toBeInTheDocument();
+    const actionsButton = screen.getByText("Actions");
+    userEvent.click(actionsButton);
+
+    const progressButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Progress",
+    });
+    expect(progressButton).toBeInTheDocument();
+    fireEvent.click(progressButton);
+
+    const url = `/teacher/student-progress/${matchedStudents_progress[0].id}`;
+    expect(push).toHaveBeenCalledWith(url);
+
+  });
+
+  it("should redirect to enroll page", async () => {
+    const matchedStudents_enroll = [
+      { id: "103186496931574456567", email: "rtclub11140@gmail.com", name: "Saeree t." },
+    ]
+
+    const push = jest.fn();
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      push
+    }));
+
+    render(
+      <MyStudents matchedStudents={matchedStudents_enroll} />
+    );
+    const myStudentsNameElement2 = screen.getByText(matchedStudents_enroll[0].name);
+    expect(myStudentsNameElement2).toBeInTheDocument();
+    const actionsButton = screen.getByText("Actions");
+    userEvent.click(actionsButton);
+
+    const enrollButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Enroll",
+    });
+    expect(enrollButton).toBeInTheDocument();
+    fireEvent.click(enrollButton);
+
+    const url = `/teacher/enroll-classes/${matchedStudents_enroll[0].id}`;
+    expect(push).toHaveBeenCalledWith(url);
+
+
+  });
+
+  it("should redirect to un-enroll page", async () => {
+    const matchedStudents_unenroll = [
+      { id: "103186496931574456567", email: "rtclub11140@gmail.com", name: "Saeree t." },
+    ];
+
+    const push = jest.fn();
+    (useRouter as jest.Mock).mockImplementation(() => ({
+      push
+    }));
+
+    render(
+      <MyStudents matchedStudents={matchedStudents_unenroll} />
+    );
+
+    const myStudentsNameElement3 = screen.getByText(matchedStudents_unenroll[0].name);
+    expect(myStudentsNameElement3).toBeInTheDocument();
+    const actionsButton = screen.getByText("Actions");
+    userEvent.click(actionsButton);
+
+    const unEnrollButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Unenroll",
+    });
+    expect(unEnrollButton).toBeInTheDocument();
+    fireEvent.click(unEnrollButton);
+
+    const url = `/teacher/unenroll-classes/${matchedStudents_unenroll[0].id}`;
+    expect(push).toHaveBeenCalledWith(url);
+
+  });
+
+  it("should popup a modal for resetting progress", async () => {
+    const matchedStudents = [
+      { id: "1", email: "test1@test.com", name: "Test Student 1" },
+    ];
+
+    render(<MyStudents matchedStudents={matchedStudents} />);
+    const myStudentsNameElement1 = screen.getByText(matchedStudents[0].name);
+    expect(myStudentsNameElement1).toBeInTheDocument();
+    const actionsButton = screen.getByText("Actions");
+    userEvent.click(actionsButton);
+
+    const resetProgressButton = await screen.findByRole("menuitemcheckbox", {
+      name: "Reset Progress",
+    });
+    expect(resetProgressButton).toBeInTheDocument();
+    userEvent.click(resetProgressButton);
+
+    const ModalPage = await screen.findByText("Reset all XP progress");
+
+    expect(ModalPage).toBeInTheDocument();
+  });
+
 });
