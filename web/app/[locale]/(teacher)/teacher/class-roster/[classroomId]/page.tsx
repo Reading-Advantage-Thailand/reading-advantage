d27d0ff762507d18c22d83c1b7a7aa7966aa64d2
Pawida Chumpurat <pawida.may@gmail.com>
2024-06-09 14:01:23 +0700

    update: optimized ClassroomData function calls in page.tsx for teacher

--- web/app/[locale]/(teacher)/teacher/class-roster/[classroomId]/page.tsx
+++ web/app/[locale]/(teacher)/teacher/class-roster/[classroomId]/page.tsx
@@ -1,6 +1,5 @@
 import ClassRoster from "@/components/teacher/class-roster";
 import React from "react";
-import { headers } from "next/headers";
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
 import { ClassroomData } from '@/lib/classroom-utils';
@@ -8,6 +7,8 @@ import { ClassroomData } from '@/lib/classroom-utils';
 export default async function RosterPage(params: {
   params: {classroomId: string;}
 }) {
+  console.log('params', params.params.classroomId);
+  
   const user = await getCurrentUser();
   if (!user) {
     return redirect("/auth/signin");
@@ -15,147 +16,6 @@ export default async function RosterPage(params: {
   if (user.role === "TEACHER") {
     return redirect("/teacher/my-classes");
   }
-
-  // //get all classroom data from database
-  // async function getAllClassroom() {
-  //   try {
-  //     const res = await fetch(
-  //       `${process.env.NEXT_PUBLIC_BASE_URL}/api/classroom/`,
-  //       {
-  //         method: "GET",
-  //         headers: headers(),
-  //       }
-  //     );
-
-  //     return res.json();
-  //   } catch (error) {
-  //     console.error("Failed to parse JSON", error);
-  //   }
-  // }
-  // const allClassroom = await getAllClassroom();
-
-  // // get student role data from database
-  // async function getAllStudentData() {
-  //   try {
-  //     const res = await fetch(
-  //       `${process.env.NEXT_PUBLIC_BASE_URL}/api/classroom/students`,
-  //       {
-  //         method: "GET",
-  //         headers: headers(),
-  //       }
-  //     );
-
-  //     return res.json();
-  //   } catch (error) {
-  //     console.error("Failed to parse JSON", error);
-  //   }
-  // }
-  // const allStudent = await getAllStudentData();
-
-  // // get teacher role data from database
-  // async function getAllTeachersData() {
-  //   try {
-  //     const res = await fetch(
-  //       `${process.env.NEXT_PUBLIC_BASE_URL}/api/classroom/teachers`,
-  //       {
-  //         method: "GET",
-  //         headers: headers(),
-  //       }
-  //     );
-
-  //     return res.json();
-  //   } catch (error) {
-  //     console.error("Failed to parse JSON", error);
-  //   }
-  // }
-  // const allTeachers = await getAllTeachersData();
-
-  // const teacherId = () => {
-  //   let teacherId: String[] = [];
-  //   allTeachers.teachers.forEach((teacher: { id: string; role: any }) => {
-  //     if (teacher.role.includes("TEACHER") && teacher.id === user.id) {
-  //       teacherId.push(teacher.id);
-  //     }
-  //   });
-  //   return teacherId;
-  // };
-  // const teacher = teacherId();
-
-  // //get student in each class
-  // const getStudentInEachClass = (classroomId: string) => {
-  //   let studentInEachClass: any[] = [];
-  //   allClassroom.data.forEach(
-  //     (classroom: {
-  //       student: any;
-  //       archived: boolean;
-  //       teacherId: string;
-  //       classroomName: string;
-  //       id: string;
-  //     }) => {
-  //       if (
-  //         !classroom.archived &&
-  //         classroom.teacherId === teacher[0] &&
-  //         classroom.id === classroomId
-  //       ) {
-  //         if (classroom.student) {
-  //           classroom.student.forEach((students: { studentId: string }) => {
-  //             studentInEachClass.push(students.studentId);
-  //           });
-  //         } else {
-  //           studentInEachClass.push("No student in this class");
-  //          }  
-  //       }
-  //     }
-  //   );
-  //   return studentInEachClass;
-  // };
-  // const studentInEachClass = getStudentInEachClass(params.params.classroomId);
-
-  // //compare student in each class and all student
-  // const getMatchedStudents = () => {
-  //   let matchedStudents: any[] = [];
-  //   allStudent.students.forEach((student: { id: string }) => {
-  //     studentInEachClass.forEach((studentId: string) => {
-  //       if (student.id === studentId) {
-  //         matchedStudents.push(student);
-  //       }
-  //     });
-  //   });
-  //   return matchedStudents;
-  // };
-  // const matchedStudents = getMatchedStudents();
-
-  // // get classroom name
-  // const getClassroomName = (classroomId: string) => {
-  //   let classrooms: any[] = [];
-  //   allClassroom.data.forEach((classroom: { id: string }) => {
-  //     if (classroom.id === classroomId) {
-  //       classrooms.push(classroom);
-  //     }
-  //   });
-  //   return classrooms;
-  // };
-  // const classrooms = getClassroomName(params.params.classroomId);
-
-  // // combine student in each class and classroom name
-  // const studentsMapped = classrooms.flatMap((classStudent) =>
-  //   classStudent.student?
-  //   classStudent.student.map(
-  //     (studentData: { studentId: string; lastActivity: any }) => {
-  //       const matchedStudent = matchedStudents.find(
-  //         (s) => s.id === studentData.studentId
-  //       );
-  //       return {
-  //         studentId: studentData.studentId,
-  //         lastActivity: studentData.lastActivity,
-  //         studentName: matchedStudent ? matchedStudent.name : "Unknown",
-  //         classroomName: classStudent.classroomName,
-  //         classroomId: classStudent.id,
-  //         email: matchedStudent ? matchedStudent.email : "Unknown",
-  //       };
-  //     }
-  //   ): []
-  // );
 
   const res = await ClassroomData({params: {classroomId: params.params.classroomId}});
   const studentsMapped = res.studentsMapped;
--- web/app/[locale]/(teacher)/teacher/my-classes/page.tsx
+++ web/app/[locale]/(teacher)/teacher/my-classes/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import MyClasses from '@/components/teacher/my-classes'
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { NextAuthSessionProvider } from "@/components/providers/nextauth-session-provider";
+import { ClassesData } from '@/lib/classroom-utils';
 
 export default async function MyClassesPage() {
   const user = await getCurrentUser();
@@ -14,65 +14,10 @@ export default async function MyClassesPage() {
     return redirect("/teacher/my-classes");
   }
 
-  // get data from database
-  async function getClassroomData() {
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/classroom`,
-        {
-          method: "GET",
-          headers: headers(),
-        }
-      );
-      return res.json();
-    } catch (error) {
-      console.error("Failed to parse JSON", error);
-    }
-  }
-const resClassroom = await getClassroomData();
 
-// get teacher data from database
-async function getAllTeachersData() {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/classroom/teachers`,
-      {
-        method: "GET",
-        headers: headers(),
-      }
-    );
-    
-    return res.json();
-  } catch (error) {
-    console.error("Failed to parse JSON", error);
-  }
-}
-const allTeachers = await getAllTeachersData();
-
-// filter only teacher login
-const teacherId = () => {
-let teacherId: String[] = [];
-allTeachers.teachers.forEach((teacher: { id: string; role: any}) => {
-  if (teacher.role && teacher.role.includes('TEACHER') && teacher.id === user.id) {
-    teacherId.push(teacher.id);
-  }
-});
-return teacherId;
-}
-const teacher = teacherId();
-
-const getClassroomOfThatTeacher = () => {
-  let classrooms: any[] = [];
-  resClassroom.data.forEach((classroom: { student: any; archived: boolean; teacherId: string; }) => {
-     if (!classroom.archived && classroom.teacherId === teacher[0]) {
-      classrooms.push(classroom);
-    }
-  });
-  return classrooms;
- }
- const classes = getClassroomOfThatTeacher();
+const res = await ClassesData();
+const classes = res.classes;
  
-
   return (
     <div>
        <NextAuthSessionProvider session={user}>
