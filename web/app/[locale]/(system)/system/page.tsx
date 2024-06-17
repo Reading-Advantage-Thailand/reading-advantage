bf20bf0eba2139e8657ddb842e19337a35c566b2
Pawida Chumpurat <pawida.may@gmail.com>
2024-06-17 15:23:20 +0700

    feat: create system page

---
+++ web/app/[locale]/(system)/system/page.tsx
@@ -0,0 +1,35 @@
+import Passages from '@/components/teacher/passages'
+import React from 'react'
+import { headers } from 'next/headers'
+import { getCurrentUser } from "@/lib/session";
+import { redirect } from "next/navigation";
+import { Header } from '@/components/header'
+
+export default async function SystemPage() {
+  const user = await getCurrentUser();
+  if (!user) {
+    return redirect("/auth/signin");
+  }
+  if (user.cefrLevel === "" && user.level === 0) {
+    return redirect("/level");
+  }
+
+  async function getPassages() {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/passage`,
+      {
+        method: "GET",
+        headers: headers(),
+      }
+    );
+    
+    return response.json();
+  }
+  const passages = await getPassages();
+  return (
+    <div className='px-[10%]'>
+      <Header heading="System Dashboard"/>
+      <Passages passages={passages.passages} />
+    </div>
+  )
+}
