c461858472d29fd17fa997d6f7f9f5c97198130c
Pawida Chumpurat <pawida.may@gmail.com>
2024-06-26 21:06:30 +0700

    update: deleting article route for system role

--- web/app/api/passage/[articleId]/route.ts
+++ web/app/api/passage/[articleId]/route.ts
@@ -1,8 +1,22 @@
 import db from "@/configs/firestore-config";
 import { authOptions } from "@/lib/auth";
+import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
+import * as z from "zod"
 
-export async function DELETE(req: Request, res: Response) {
+const routeContextSchema = z.object({
+    params: z.object({
+        articleId: z.string(),
+    }),
+})
+
+export async function DELETE(req: Request, context: z.infer<typeof routeContextSchema>) {    
+console.log('DELETE article');
+
+const { params } = routeContextSchema.parse(context);
+const articleId = params.articleId;
+    console.log('articleId', articleId);
+    
     try {
         const session = await getServerSession(authOptions);
         if (!session) {
@@ -16,10 +30,23 @@ export async function DELETE(req: Request, res: Response) {
         
         const userId = session.user.id;
         const userRole = session.user.role;
-        // const articleId = req.params.id;
-        const articlesRef = db.collection('new-articles');
-        const snapshot = await articlesRef.get();
-        const articles = snapshot.docs.map(doc => doc.data());
+       const articlesRef = db.collection('new-articles');
+       const articleToDelete = await articlesRef.doc(articleId).get();
+       console.log('articleToDelete', articleToDelete);
+       // const snapshot = await articlesRef.get();
+       // const articles = snapshot.docs.map(doc => doc.data());
+       
+       if (userId && userRole.includes("SYSTEM")) {
+        if (!articleToDelete.exists) {
+return new Response(JSON.stringify({ message: "No such article found" }), { status: 404 })
+        }else {
+await articlesRef.doc(articleId).delete();
+console.log('Document successfully deleted');
+return new Response(JSON.stringify({ message: "Article deleted" }), { status: 200 })
+        }
+       
+    }
+            
 
         if (!userRole.includes("SYSTEM")) {
         return new Response(
