a841a0d036abb9c43af03f9c86dec4f40beacc03
Pawida Chumpurat <pawida.may@gmail.com>
2024-07-16 14:50:03 +0700

    feat: create system dashboard page

---
+++ web/app/[locale]/(system)/system/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import ActivityDistributionPieChart from '@/components/system/activity-distribution-pieChart'
+import React from 'react'
+
+export default async function SystemDashboardPage() {
+   
+  return (
+    <>
+    <div>
+      system dashboard page
+    </div>
+ {/* <ActivityDistributionPieChart /> */}
+    </>
+  )
+}
+
