508fc1ab58e8aac6672272997e3ee18c91957a0b
Pawida Chumpurat <pawida.may@gmail.com>
2024-07-21 20:05:27 +0700

    refactor: Move SystemPage to handle-passages and switch from Passages to System component

--- web/app/[locale]/(system)/system/handle-passages/page.tsx
+++ web/app/[locale]/(system)/system/handle-passages/page.tsx
@@ -1,11 +1,17 @@
-import Passages from '@/components/teacher/passages'
-import React from 'react'
-import { headers } from 'next/headers'
+import Passages from "@/components/teacher/passages";
+import React from "react";
+import { headers } from "next/headers";
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
-import { Header } from '@/components/header'
+import { Header } from "@/components/header";
+import System from "@/components/system-articles";
+import { fetchMoreArticles } from "@/lib/fetchMoreArticles";
 
-export default async function HandlePassagesPage() {
+export default async function SystemPage({
+  searchParams,
+}: {
+  searchParams: URLSearchParams;
+}) {
   const user = await getCurrentUser();
   if (!user) {
     return redirect("/auth/signin");
@@ -14,30 +20,29 @@ export default async function HandlePassagesPage() {
     return redirect("/level");
   }
 
-  async function getPassages() {
-    try{
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/passage`,
-        {
-          method: "GET",
-          headers: headers(),
-        }
-      );
-      const res = await response.json();
-      return res;
-      // return res.data;
-    } catch (error) {
-      console.error('failed to fetch passages: ', error);
-      return [];
-    }
+  // const params =
+  //   searchParams instanceof URLSearchParams
+  //     ? searchParams
+  //     : new URLSearchParams(searchParams);
+  // const lastDocId = params.get("lastDocId") || "";
+  // const typeParams = params.get("type") || "";
+  // const genreParams = params.get("genre") || "";
+  // const subgenreParams = params.get("subgenre") || "";
+  // const levelParams = params.get("level") || "";
+  // const searchTermParams = params.get("searchTerm") || "";
 
-  }
-  const passages = await getPassages();
-  
+  // console.log("==================pages.tsx==================");
+  // console.log("lastDocId: ", lastDocId);
+  // console.log("typeParams: ", typeParams);
+  // console.log("genreParams: ", genreParams);
+  // console.log("subgenreParams: ", subgenreParams);
+  // console.log("levelParams: ", levelParams);
+  // console.log("searchTermParams: ", searchTermParams);
+
   return (
-    <div>
-      <Header heading="System Dashboard"/>
-      <Passages passages={passages.passages}  />
+    <div className="px-[10%]">
+      <Header heading="System Dashboard" />
+      <System fetchMoreData={fetchMoreArticles} />
     </div>
-  )
+  );
 }
