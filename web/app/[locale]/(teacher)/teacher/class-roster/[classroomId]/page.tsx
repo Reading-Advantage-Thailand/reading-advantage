1bdfa396d71c4208a08b606ab2ba95b7f094dc7a
Pawida Chumpurat <pawida.may@gmail.com>
2024-08-22 19:52:58 +0700

    add: Add check to skip filtering for empty studentId

--- web/app/[locale]/(teacher)/teacher/class-roster/[classroomId]/page.tsx
+++ web/app/[locale]/(teacher)/teacher/class-roster/[classroomId]/page.tsx
@@ -12,8 +12,9 @@ async function getUserArticleRecords(userId: string) {
 
 export default async function RosterPage(params: {
   params: {
-    studentId: string; classroomId: string 
-};
+    studentId: string;
+    classroomId: string;
+  };
 }) {
   const user = await getCurrentUser();
   if (!user) {
@@ -30,41 +31,50 @@ export default async function RosterPage(params: {
   const classes = classesRes.classes;
 
   const lastActivityTimestamp: { [key: string]: string } = {};
-  
+
   for (let i = 0; i < studentsMapped.length; i++) {
     const studentId = studentsMapped[i].studentId;
-    const results = await getUserArticleRecords(studentId);
-    
- const userRecordsMatch = results.results.filter((record: any) => record.userId === studentId);
 
-if (userRecordsMatch.length > 0) {
-  const sortedData = results.results.sort((a: any, b: any) => {
-    const timestampA = new Date(a.timestamp).getTime();
-    const timestampB = new Date(b.timestamp).getTime();
-    return timestampB - timestampA;
-  }).map((item: any) => {
-    const date = new Date(item.timestamp);
-    const formattedDate = date.toISOString().split('T')[0]; // Extracts "yyyy-mm-dd"
-    return {
-      ...item,
-      formattedTimestamp: formattedDate
-    };
-  });
-  
-  const lasttimestamp = sortedData[0].formattedTimestamp;
-  lastActivityTimestamp[studentId] = lasttimestamp;
-} else {
-  lastActivityTimestamp[studentId] = 'No Activity';
-}
-  };
-  
-  const sortedLastActivityTimestamp = Object.entries(lastActivityTimestamp).sort(
-    ([timestampA], [timestampB]) => {
-      if (timestampA === 'No Activity') return 1;
-      if (timestampB === 'No Activity') return -1;
-      return new Date(timestampB).getTime() - new Date(timestampA).getTime();
+    if (!studentId) {
+      continue;
     }
-  );
+
+    const results = await getUserArticleRecords(studentId);
+
+    const userRecordsMatch = results.results.filter(
+      (record: any) => record.userId === studentId
+    );
+
+    if (userRecordsMatch.length > 0) {
+      const sortedData = results.results
+        .sort((a: any, b: any) => {
+          const timestampA = new Date(a.timestamp).getTime();
+          const timestampB = new Date(b.timestamp).getTime();
+          return timestampB - timestampA;
+        })
+        .map((item: any) => {
+          const date = new Date(item.timestamp);
+          const formattedDate = date.toISOString().split("T")[0];
+          return {
+            ...item,
+            formattedTimestamp: formattedDate,
+          };
+        });
+
+      const lasttimestamp = sortedData[0].formattedTimestamp;
+      lastActivityTimestamp[studentId] = lasttimestamp;
+    } else {
+      lastActivityTimestamp[studentId] = "No Activity";
+    }
+  }
+
+  const sortedLastActivityTimestamp = Object.entries(
+    lastActivityTimestamp
+  ).sort(([timestampA], [timestampB]) => {
+    if (timestampA === "No Activity") return 1;
+    if (timestampB === "No Activity") return -1;
+    return new Date(timestampB).getTime() - new Date(timestampA).getTime();
+  });
 
   return (
     <div>
