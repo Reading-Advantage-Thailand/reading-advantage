1222f98aec9eab681e41e31ac278db19dfe63297
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2024-08-24 22:58:44 +0700

    Revert "Merge pull request #153 from bodangren/pawida/fix-last-activity"
    This reverts commit c869af46b3101d689ba98050e4cb50d82d9da21c, reversing
    changes made to 7f12c4c52ea95756ee5f8730af9f6f173741ed3c.

--- web/app/[locale]/(teacher)/teacher/reports/[classroomId]/page.tsx
+++ web/app/[locale]/(teacher)/teacher/reports/[classroomId]/page.tsx
@@ -5,11 +5,6 @@ import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
 import ClassroomData from "@/lib/classroom-utils";
 import { ClassesData } from "@/lib/classroom-utils";
-import { fetchData } from "@/utils/fetch-data";
-
-async function getUserArticleRecords(userId: string) {
-  return fetchData(`/api/v1/users/${userId}/activitylog`);
-}
 
 export default async function ReportsClassroomPage(params: {
   params: { classroomId: string };
@@ -28,52 +23,6 @@ export default async function ReportsClassroomPage(params: {
   const classesRes = await ClassesData();
   const classes = classesRes.classes;
 
-  const lastActivityTimestamp: { [key: string]: string } = {};
-
-  for (let i = 0; i < studentsMapped.length; i++) {
-    const studentId = studentsMapped[i].studentId;
-
-    if (!studentId) {
-      continue;
-    }
-
-    const results = await getUserArticleRecords(studentId);
-
-    const userRecordsMatch = results.results.filter(
-      (record: any) => record.userId === studentId
-    );
-
-    if (userRecordsMatch.length > 0) {
-      const sortedData = results.results
-        .sort((a: any, b: any) => {
-          const timestampA = new Date(a.timestamp).getTime();
-          const timestampB = new Date(b.timestamp).getTime();
-          return timestampB - timestampA;
-        })
-        .map((item: any) => {
-          const date = new Date(item.timestamp);
-          const formattedDate = date.toISOString().split("T")[0];
-          return {
-            ...item,
-            formattedTimestamp: formattedDate,
-          };
-        });
-
-      const lasttimestamp = sortedData[0].formattedTimestamp;
-      lastActivityTimestamp[studentId] = lasttimestamp;
-    } else {
-      lastActivityTimestamp[studentId] = "No Activity";
-    }
-  }
-
-  const sortedLastActivityTimestamp = Object.entries(
-    lastActivityTimestamp
-  ).sort(([timestampA], [timestampB]) => {
-    if (timestampA === "No Activity") return 1;
-    if (timestampB === "No Activity") return -1;
-    return new Date(timestampB).getTime() - new Date(timestampA).getTime();
-  });
-
   return (
     <div>
       <NextAuthSessionProvider session={user}>
@@ -82,7 +31,6 @@ export default async function ReportsClassroomPage(params: {
           userId={user.id}
           classrooms={classrooms}
           classes={classes}
-          userArticleRecords={sortedLastActivityTimestamp}
         />
       </NextAuthSessionProvider>
     </div>
