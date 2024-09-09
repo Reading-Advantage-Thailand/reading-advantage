bddfa8a083e1ef9f2b36eeaf93a86bee3c139b87
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2024-09-09 19:43:20 +0700

    fix:license page fetch url error

--- web/server/handlers/handler-factory.ts
+++ web/server/handlers/handler-factory.ts
@@ -7,61 +7,84 @@ import { DocumentData } from "firebase/firestore";
 import createFirestoreService from "../services/create-firestore-service";
 
 export const getOne = <T extends DocumentData>(collection: DBCollection) =>
-    catchAsync(async (req: ExtendedNextRequest, { params: { id } }: { params: { id: string } }) => {
-        const service = createFirestoreService<T>(collection);
-        const doc = await service.getDoc(id);
+  catchAsync(
+    async (
+      req: ExtendedNextRequest,
+      { params: { id } }: { params: { id: string } }
+    ) => {
+      const service = createFirestoreService<T>(collection);
+      const doc = await service.getDoc(id);
 
-        if (!doc) {
-            return NextResponse.json({
-                message: "Document not found",
-            }, { status: 404 });
-        }
+      if (!doc) {
+        return NextResponse.json(
+          {
+            message: "Document not found",
+          },
+          { status: 404 }
+        );
+      }
 
-        return NextResponse.json({
-            data: doc,
-        });
-    });
+      return NextResponse.json({
+        data: doc,
+      });
+    }
+  );
 
 export const updateOne = <T extends DocumentData>(collection: DBCollection) =>
-    catchAsync(async (req: ExtendedNextRequest, { params: { id } }: { params: { id: string } }) => {
-        const body = await req.json();
-        if (!body) {
-            return NextResponse.json({
-                message: "No data to update",
-            }, { status: 400 });
-        }
-        const service = createFirestoreService<T>(collection);
-        await service.updateDoc(id, body);
+  catchAsync(
+    async (
+      req: ExtendedNextRequest,
+      { params: { id } }: { params: { id: string } }
+    ) => {
+      const body = await req.json();
+      if (!body) {
+        return NextResponse.json(
+          {
+            message: "No data to update",
+          },
+          { status: 400 }
+        );
+      }
+      const service = createFirestoreService<T>(collection);
+      await service.updateDoc(id, body);
 
-        return NextResponse.json({
-            message: "Document updated",
-            data: body,
-        });
-    });
+      return NextResponse.json({
+        message: "Document updated",
+        data: body,
+      });
+    }
+  );
 
 export const deleteOne = <T extends DocumentData>(collection: DBCollection) =>
-    catchAsync(async (req: ExtendedNextRequest, { params: { id } }: { params: { id: string } }) => {
-        const service = createFirestoreService<T>(collection);
-        await service.deleteDoc(id)
+  catchAsync(
+    async (
+      req: ExtendedNextRequest,
+      { params: { id } }: { params: { id: string } }
+    ) => {
+      const service = createFirestoreService<T>(collection);
+      await service.deleteDoc(id);
 
-        return NextResponse.json({
-            message: "Document deleted",
-        });
-    });
+      return NextResponse.json({
+        message: "Document deleted",
+      });
+    }
+  );
 
 export const getAlls = <T extends DocumentData>(collection: DBCollection) =>
-    catchAsync(async (req: ExtendedNextRequest) => {
-        const filter = Object.fromEntries(req.nextUrl.searchParams.entries());
-        const service = createFirestoreService<T>(collection);
-        const docs = await service.getAllDocs({
-            select: filter.select ? JSON.parse(decodeURIComponent(filter.select)) : undefined,
-        });
-
-        return NextResponse.json({
-            length: docs.length,
-            data: docs,
-        });
+  catchAsync(async (req: ExtendedNextRequest) => {
+    const filter = Object.fromEntries(req.nextUrl.searchParams.entries());
+    const service = createFirestoreService<T>(collection);
+    const docs = await service.getAllDocs({
+      select: filter.select
+        ? JSON.parse(decodeURIComponent(filter.select))
+        : undefined,
     });
+
+    return NextResponse.json({
+      length: docs.length,
+      data: docs,
+    });
+  });
 
 // export const filtered = <T extends DocumentData>(collection: DBCollection) =>
 //     catchAsync(async (req: ExtendedNextRequest) => {
