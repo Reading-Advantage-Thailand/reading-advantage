f31dd94b4939ba5eb9c44e66595bb6f001db4cd2
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2024-10-14 18:51:25 +0700

    feat:add feature update license user

--- web/app/api/v1/licenses/[id]/route.ts
+++ web/app/api/v1/licenses/[id]/route.ts
@@ -1,25 +1,39 @@
 import { restrictTo } from "@/server/controllers/auth-controller";
-import { deleteLicense } from "@/server/controllers/license-controller";
+import {
+  deleteLicense,
+  activateLicense,
+} from "@/server/controllers/license-controller";
 import { logRequest } from "@/server/middleware";
 import { Role } from "@/server/models/enum";
 import { handleRequest } from "@/server/utils/handle-request";
 import { createEdgeRouter } from "next-connect";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export interface Context {
+export interface RequestContext {
   params?: {
     id: string;
   };
 }
 
-const router = createEdgeRouter<NextRequest, Context>();
+const router = createEdgeRouter<NextRequest, RequestContext>();
 
 // Middleware
 router.use(logRequest);
-router.use(restrictTo(Role.SYSTEM));
+//router.use(restrictTo(Role.SYSTEM));
 
 // /api/license/[id]
+router.patch(activateLicense);
 router.delete(deleteLicense);
 
-export const DELETE = (request: NextRequest, ctx: Context) =>
+export async function PATCH(request: NextRequest, ctx: RequestContext) {
+  const result = await router.run(request, ctx);
+  if (result instanceof NextResponse) {
+    return result;
+  }
+  // Handle the case where result is not a NextResponse
+  // You might want to return a default NextResponse or throw an error
+  throw new Error("Expected a NextResponse from router.run");
+}
+
+export const DELETE = (request: NextRequest, ctx: RequestContext) =>
   handleRequest(router, request, ctx);
--- web/server/controllers/license-controller.ts
+++ web/server/controllers/license-controller.ts
@@ -5,25 +5,101 @@ import { licenseService } from "../services/firestore-server-services";
 import { createLicenseModel, License } from "../models/license";
 import { DBCollection } from "../models/enum";
 import { getAlls, deleteOne } from "../handlers/handler-factory";
+import db from "@/configs/firestore-config";
+
+interface RequestContext {
+  params: {
+    id: string;
+  };
+}
 
 export const createLicenseKey = catchAsync(async (req: ExtendedNextRequest) => {
-    const { total_licenses, subscription_level, school_name, admin_id } = await req.json();
-    const license: Omit<License, "id"> = createLicenseModel({
-        totalLicense: total_licenses,
-        subscriptionLevel: subscription_level,
-        userId: req.session?.user.id || "",
-        adminId: admin_id,
-        schoolName: school_name,
-    })
-    await licenseService.licenses.createDoc(license);
-    return NextResponse.json({
-        message: "License key created successfully",
-        license,
-    });
+  const { total_licenses, subscription_level, school_name, admin_id } =
+    await req.json();
+  const license: Omit<License, "id"> = createLicenseModel({
+    totalLicense: total_licenses,
+    subscriptionLevel: subscription_level,
+    userId: req.session?.user.id || "",
+    adminId: admin_id,
+    schoolName: school_name,
+  });
+  await licenseService.licenses.createDoc(license);
+  return NextResponse.json({
+    message: "License key created successfully",
+    license,
+  });
 });
 
 export const getAllLicenses = getAlls<License>(DBCollection.LICENSES);
 export const deleteLicense = deleteOne<License>(DBCollection.LICENSES);
+
+export const activateLicense = async (req: ExtendedNextRequest) => {
+  try {
+    const { key, userId } = await req.json();
+    const license = await db
+      .collection(DBCollection.LICENSES)
+      .where("key", "==", key)
+      .get();
+
+    const licenseData = license.docs.map((license) => license.data());
+
+    if (license.empty) {
+      return NextResponse.json(
+        {
+          message: "License not found",
+        },
+        { status: 404 }
+      );
+    } else if (licenseData[0].total_licenses <= licenseData[0].used_licenses) {
+      return NextResponse.json(
+        {
+          message: "License is already used",
+        },
+        { status: 404 }
+      );
+    }
+
+    //update user expired date
+    const user = await db.collection(DBCollection.USERS).doc(userId).get();
+    const userData = user.data();
+    if (!userData) {
+      return NextResponse.json(
+        {
+          message: "User data not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    if (!license.empty && userData) {
+      const licenseUpdate = await db
+        .collection(DBCollection.LICENSES)
+        .doc(licenseData[0].id)
+        .update({
+          used_licenses: licenseData[0].used_licenses + 1,
+        });
+
+      const userUpdate = await db
+        .collection(DBCollection.USERS)
+        .doc(userId)
+        .update({
+          expired_date: licenseData[0].expiration_date,
+          license_id: licenseData[0].id,
+        });
+
+      return NextResponse.json(
+        { message: "License activated successfully" },
+        { status: 200 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({
+      message: "Internal server error",
+      status: 500,
+    });
+  }
+};
+
 // export const getFilteredLicenses = catchAsync(async (req: ExtendedNextRequest) => {
 //     // req.nextUrl.searchParams.get("page");
 //     // URLSearchParams { 'page' => '1' }
@@ -102,7 +178,6 @@ export const deleteLicense = deleteOne<License>(DBCollection.LICENSES);
 //         message: "Licenses fetched successfully",
 //     });
 // });
-
 
 // export const getAllLicenses = catchAsync(async (req: ExtendedNextRequest, { params: { id } }: { params: { id: string } }) => {
 //     const licenses = await db.collection(DBCollection.LICENSES).where("school_id", "==", id).get();
