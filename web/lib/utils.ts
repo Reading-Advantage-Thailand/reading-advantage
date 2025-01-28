c758f07129c842dd61e7dda6bc5301558ae5e9c1
Jaturapat Dusadde <mr.jaturapat.d@gmail.com>
2025-01-29 02:33:39 +0700

    fix:fix date in sentences manage tab

--- web/lib/utils.ts
+++ web/lib/utils.ts
@@ -11,25 +11,64 @@ export function formatDate(updatedAt: string): string {
   const date = new Date(updatedAt);
   const now = new Date();
   const diff = now.getTime() - date.getTime();
-  const seconds = Math.floor(diff / 1000);
+  const absDiff = Math.abs(diff);
+  const seconds = Math.floor(absDiff / 1000);
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
   const months = Math.floor(days / 30);
   const years = Math.floor(months / 12);
 
+  const isFuture = diff < 0;
+
   if (years > 0) {
-    return years === 1 ? "1 year ago" : `${years} years ago`;
+    return isFuture
+      ? years === 1
+        ? "In the next 1 year"
+        : `In the next ${years} years`
+      : years === 1
+      ? "1 year ago"
+      : `${years} years ago`;
   } else if (months > 0) {
-    return months === 1 ? "1 month ago" : `${months} months ago`;
+    return isFuture
+      ? months === 1
+        ? "In the next 1 month"
+        : `In the next ${months} months`
+      : months === 1
+      ? "1 month ago"
+      : `${months} months ago`;
   } else if (days > 0) {
-    return days === 1 ? "1 day ago" : `${days} days ago`;
+    return isFuture
+      ? days === 1
+        ? "In the next 1 day"
+        : `In the next ${days} days`
+      : days === 1
+      ? "1 day ago"
+      : `${days} days ago`;
   } else if (hours > 0) {
-    return hours === 1 ? "1 hour ago" : `${hours} hours ago`;
+    return isFuture
+      ? hours === 1
+        ? "In the next 1 hour"
+        : `In the next ${hours} hours`
+      : hours === 1
+      ? "1 hour ago"
+      : `${hours} hours ago`;
   } else if (minutes > 0) {
-    return minutes === 1 ? "1 minute ago" : `${minutes} minutes ago`;
+    return isFuture
+      ? minutes === 1
+        ? "In the next 1 minute"
+        : `In the next ${minutes} minutes`
+      : minutes === 1
+      ? "1 minute ago"
+      : `${minutes} minutes ago`;
   } else {
-    return seconds === 1 ? "1 second ago" : `${seconds} seconds ago`;
+    return isFuture
+      ? seconds === 1
+        ? "In the next 1 second"
+        : `In the next ${seconds} seconds`
+      : seconds === 1
+      ? "1 second ago"
+      : `${seconds} seconds ago`;
   }
 }
 export function formatTimestamp(updatedAt: {
