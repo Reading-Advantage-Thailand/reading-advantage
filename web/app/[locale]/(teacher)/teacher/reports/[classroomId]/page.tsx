a32f64201e73d9f1ebf01fc651d52ec8007f7fac
Bunyawat Singkiporn <pupahero@gmail.com>
2025-02-06 16:27:55 +0700

    fix: class selector issues in reports and class roster page

--- web/app/[locale]/(teacher)/teacher/reports/[classroomId]/page.tsx
+++ web/app/[locale]/(teacher)/teacher/reports/[classroomId]/page.tsx
@@ -1,10 +1,11 @@
 import Reports from "@/components/teacher/reports";
-import React from "react";
 import { getCurrentUser } from "@/lib/session";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 
-export default async function ReportsClassroomPage(params: {
+export default async function ReportsClassroomPage({
+  params,
+}: {
   params: { classroomId: string };
 }) {
   const user = await getCurrentUser();
@@ -15,21 +16,35 @@ export default async function ReportsClassroomPage(params: {
   const ClassesData = async () => {
     const resClass = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/classroom`,
-      { method: "GET", headers: headers() }
+      {
+        method: "GET",
+        headers: headers(),
+      }
     );
     if (!resClass.ok) throw new Error("Failed to fetch ClassesData list");
     const ClassroomData = await resClass.json();
 
+    const allClassrooms =
+      user.role === "system"
+        ? ClassroomData.data
+        : ClassroomData.data.filter(
+            (classroom: { teacherId: string }) =>
+              classroom.teacherId === user.id
+          );
+
+    const classData = allClassrooms.filter(
+      (classroom: { id: string }) => classroom.id === params.classroomId
+    );
+
     const resStudent = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/classroom/students`,
-      { method: "GET", headers: headers() }
+      {
+        method: "GET",
+        headers: headers(),
+      }
     );
     if (!resStudent.ok) throw new Error("Failed to fetch StudentData list");
     const studentsData = await resStudent.json();
-
-    const classData = ClassroomData.data.filter(
-      (classroom: { id: string }) => classroom.id === params.params.classroomId
-    );
 
     const StudentId: string[] = classData.flatMap((classroom: any) =>
       classroom.student.map((student: any) => student.studentId)
@@ -39,7 +54,16 @@ export default async function ReportsClassroomPage(params: {
       (entry: { id: string }) => StudentId.includes(entry.id)
     );
 
-    return { classData, studentInClass };
+    const classes = allClassrooms.map((classroom: any) => ({
+      id: classroom.id,
+      classroomName: classroom.classroomName,
+    }));
+
+    return {
+      classData,
+      studentInClass,
+      classes,
+    };
   };
 
   const data = await ClassesData();
@@ -49,6 +73,7 @@ export default async function ReportsClassroomPage(params: {
       <Reports
         studentInClass={data.studentInClass}
         classrooms={data.classData}
+        classes={data.classes}
       />
     </div>
   );
