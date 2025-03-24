b760ede3198c576320b47456a596e3c8cfc1400e
OLEANG <wannachok.job@gmail.com>
2025-03-24 21:42:57 +0700

    fix: merge api to update admin dashboard and send discord web hook

--- web/app/api/v1/admin/update-admin-dashboard/route.ts
+++ web/app/api/v1/admin/update-admin-dashboard/route.ts
@@ -1,25 +1,26 @@
 import { logRequest } from "@/server/middleware";
 import { createEdgeRouter } from "next-connect";
 import { NextResponse, type NextRequest } from "next/server";
-import { articlesByTypeGenre } from "@/server/controllers/article-controller";
+import { updateAdminDashboard } from "@/server/controllers/update-admin-dashboard-controller";
 import { restrictAccessKey } from "@/server/controllers/auth-controller";
 
-export interface RequestContext {
+export interface ExtendedNextRequest {
   params?: {
-    license_id: string;
+    licenseId: string;
+    classroomId: string;
   };
 }
 
-const router = createEdgeRouter<NextRequest, RequestContext>();
+const router = createEdgeRouter<NextRequest, ExtendedNextRequest>();
 
 // Middleware
 router.use(logRequest);
 router.use(restrictAccessKey);
 
-// API: POST api/v1/system/dashboard/updateArticleByTypeGenre
-router.post(articlesByTypeGenre);
+// API: POST api/v1/admin/update-admin-dashboard
+router.post(updateAdminDashboard);
 
-export async function POST(request: NextRequest, ctx: RequestContext) {
+export async function POST(request: NextRequest, ctx: ExtendedNextRequest) {
   const result = await router.run(request, ctx);
   if (result instanceof NextResponse) {
     return result;
