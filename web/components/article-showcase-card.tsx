19658825657eb4392dfea09e50190713798e0000
OLEANG <wannachok.job@gmail.com>
2025-04-23 20:09:38 +0700

    fix: change play button as per requirement

--- web/components/article-showcase-card.tsx
+++ web/components/article-showcase-card.tsx
@@ -31,6 +31,7 @@ async function getTranslateSentence(
 const ArticleShowcaseCard = React.forwardRef<HTMLDivElement, Props>(
   ({ article, userId }, ref) => {
     const [summarySentence, setSummarySentence] = React.useState<string[]>([]);
+    const [playToggle, setPlayToggle] = React.useState(false);
     const locale = useCurrentLocale();
     const pathName = usePathname();
     const systemPathRegex = /\/(?:[a-z]{2}\/)?system\/.*\/?$/i;
@@ -67,9 +68,87 @@ const ArticleShowcaseCard = React.forwardRef<HTMLDivElement, Props>(
         className="relative hover:scale-105 transition-all duration-300"
         ref={ref}
       >
+        {/* ปุ่ม ▶ + Dropdown Menu พร้อม Animation */}
         <div
-          className="absolute top-2 right-2 z-10"
+          className={`group absolute top-2 right-2 z-10 transition-transform duration-300 ${
+            article.is_read ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
+          }`}
+        >
+          <div
+            className={`text-[30px] text-white rounded transition-transform duration-300 ${
+              playToggle ? "rotate-180" : "rotate-0"
+            }`}
+            onClick={() => setPlayToggle(!playToggle)}
+          >
+            {playToggle ? "❌" : "▶"}
+          </div>
+          <div
+            className={`absolute top-full mt-2 right-0 w-max flex flex-col rounded text-sm z-20 transition-transform duration-300 gap-5 ${
+              playToggle ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            {playToggle && (
+              <div className="flex flex-col gap-2 p-2">
+                <Link
+                  href={`/student/lesson/${article.id}`}
+                  onClick={() =>
+                    fetch(`/api/v1/users/${userId}/activitylog`, {
+                      method: "POST",
+                      headers: { "Content-Type": "application/json" },
+                      body: JSON.stringify({
+                        articleId: article.id,
+                        activityType: ActivityType.LessonRead,
+                        activityStatus: ActivityStatus.InProgress,
+                        details: {
+                          title: article.title,
+                          level: article.ra_level,
+                          cefr_level: article.cefr_level,
+                          type: article.type,
+                          genre: article.genre,
+                          subgenre: article.subgenre,
+                        },
+                      }),
+                    })
+                  }
+                  className="hover:bg-red-600 bg-red-800 text-[12px] font-semibold p-1 rounded-md"
+                >
+                  ▶ Study as 45-min Lesson
+                </Link>
+                <Link
+                  href={`/student/read/${article.id}`}
+                  onClick={() =>
+                    fetch(`/api/v1/users/${userId}/activitylog`, {
+                      method: "POST",
+                      headers: { "Content-Type": "application/json" },
+                      body: JSON.stringify({
+                        articleId: article.id,
+                        activityType: ActivityType.ArticleRead,
+                        activityStatus: ActivityStatus.InProgress,
+                        details: {
+                          title: article.title,
+                          level: article.ra_level,
+                          cefr_level: article.cefr_level,
+                          type: article.type,
+                          genre: article.genre,
+                          subgenre: article.subgenre,
+                        },
+                      }),
+                    })
+                  }
+                  className="hover:bg-red-600 bg-red-800 text-[12px] font-semibold p-1 rounded-md"
+                >
+                  📖 Read article
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+        <div
+          ref={ref}
+          className="w-full flex flex-col gap-1 h-[20rem] bg-cover bg-center p-3 rounded-md bg-black "
           style={{
+            backgroundImage: `url('https://storage.googleapis.com/artifacts.reading-advantage.appspot.com/images/${article.id}.png')`,
+            boxShadow: "inset 80px 10px 90px 10px rgba(0, 0, 0, 0.9)",
             opacity:
               article.is_read ||
               (article.is_approved && systemPathRegex.test(pathName))
@@ -77,142 +156,60 @@ const ArticleShowcaseCard = React.forwardRef<HTMLDivElement, Props>(
                 : 1,
           }}
         >
-          <Link
-            href={`/student/lesson/${article.id}`}
-            onClick={() =>
-              fetch(`/api/v1/users/${userId}/activitylog`, {
-                method: "POST",
-                body: JSON.stringify({
-                  articleId: article.id,
-                  activityType: ActivityType.LessonRead,
-                  activityStatus: ActivityStatus.InProgress,
-                  details: {
-                    title: article.title,
-                    level: article.ra_level,
-                    cefr_level: article.cefr_level,
-                    type: article.type,
-                    genre: article.genre,
-                    subgenre: article.subgenre,
-                  },
-                }),
-              })
-            }
-          >
-            <div
-              className={`group relative inline-flex overflow-hidden transition-all duration-1000 ease-in-out ${
-                article.is_read
-                  ? "opacity-50 cursor-not-allowed"
-                  : "cursor-pointer"
-              }`}
-            >
-              <span
-                className="bg-red-800 text-white pl-2 py-1 flex items-center space-x-2 transition-all duration-300 ease-in-out
-                   group-hover:pr-6 group-hover:pl-3 rounded shadow-md"
-              >
-                <span className="transition-all duration-300 ease-in-out">
-                  ▶
-                </span>
-                <span className="whitespace-nowrap overflow-hidden max-w-0 group-hover:max-w-xs group-hover:ml-2 transition-all duration-300 ease-in-out text-[12px] font-semibold">
-                  Study as 45-min Lesson
-                </span>
-              </span>
+          {article.ra_level && (
+            <Badge className="shadow-lg max-w-max" variant="destructive">
+              Reading Advantage Level: {article.ra_level}
+            </Badge>
+          )}
+          <Badge className="shadow-lg max-w-max" variant="destructive">
+            CEFR Level: {article.cefr_level}
+          </Badge>
+          <Badge className="shadow-lg max-w-max" variant="destructive">
+            {t(article.genre)}, {t(article.subgenre)}
+          </Badge>
+          <Badge className="shadow-lg max-w-max" variant="destructive">
+            <Rating name="read-only" value={article.average_rating} readOnly />
+          </Badge>
+          <div className="mt-auto">
+            <div className=" bg-black bg-opacity-40">
+              <p className="text-xl drop-shadow-lg font-bold text-white">
+                {article.title}
+              </p>
             </div>
-          </Link>
-        </div>
-
-        <Link
-          href={`/student/read/${article.id}`}
-          onClick={() =>
-            fetch(`/api/v1/users/${userId}/activitylog`, {
-              method: "POST",
-              body: JSON.stringify({
-                articleId: article.id,
-                activityType: ActivityType.ArticleRead,
-                activityStatus: ActivityStatus.InProgress,
-                details: {
-                  title: article.title,
-                  level: article.ra_level,
-                  cefr_level: article.cefr_level,
-                  type: article.type,
-                  genre: article.genre,
-                  subgenre: article.subgenre,
-                },
-              }),
-            })
-          }
-        >
-          <div
-            ref={ref}
-            className="w-full flex flex-col gap-1 h-[20rem] bg-cover bg-center p-3 rounded-md bg-black "
-            style={{
-              backgroundImage: `url('https://storage.googleapis.com/artifacts.reading-advantage.appspot.com/images/${article.id}.png')`,
-              boxShadow: "inset 80px 10px 90px 10px rgba(0, 0, 0, 0.9)",
-              opacity:
-                article.is_read ||
-                (article.is_approved && systemPathRegex.test(pathName))
-                  ? 0.3
-                  : 1,
-            }}
-          >
-            {article.ra_level && (
-              <Badge className="shadow-lg max-w-max" variant="destructive">
-                Reading Advantage Level: {article.ra_level}
-              </Badge>
-            )}
-            <Badge className="shadow-lg max-w-max" variant="destructive">
-              CEFR Level: {article.cefr_level}
-            </Badge>
-            <Badge className="shadow-lg max-w-max" variant="destructive">
-              {t(article.genre)}, {t(article.subgenre)}
-            </Badge>
-            <Badge className="shadow-lg max-w-max" variant="destructive">
-              <Rating
-                name="read-only"
-                value={article.average_rating}
-                readOnly
-              />
-            </Badge>
-            <div className="mt-auto">
-              <div className=" bg-black bg-opacity-40">
-                <p className="text-xl drop-shadow-lg font-bold text-white">
-                  {article.title}
-                </p>
-              </div>
-              <div className=" bg-black bg-opacity-40">
-                <p className="text-sm drop-shadow-lg line-clamp-4 text-white">
-                  {locale == "en" ? (
-                    <p>{article.summary}</p>
-                  ) : (
-                    <p>{summarySentence}</p>
-                  )}
-                </p>
-              </div>
+            <div className=" bg-black bg-opacity-40">
+              <p className="text-sm drop-shadow-lg line-clamp-4 text-white">
+                {locale == "en" ? (
+                  <React.Fragment>{article.summary}</React.Fragment>
+                ) : (
+                  <React.Fragment>{summarySentence}</React.Fragment>
+                )}
+              </p>
             </div>
           </div>
-          {article.is_read && !article.is_completed && (
-            <div className="flex justify-center">
-              <Badge className="relative m-auto -top-[11rem] text-md left-0 right-0 shadow-lg max-w-max bg-slate-200 text-slate-900">
-                Started
-              </Badge>
-            </div>
-          )}
+        </div>
+        {article.is_read && !article.is_completed && (
+          <div className="flex justify-center">
+            <Badge className="relative m-auto -top-[11rem] text-md left-0 right-0 shadow-lg max-w-max bg-slate-200 text-slate-900">
+              Started
+            </Badge>
+          </div>
+        )}
 
-          {article.is_read && article.is_completed && (
-            <div className="flex justify-center">
-              <Badge className="relative m-auto -top-[11rem] text-md left-0 right-0 shadow-lg max-w-max bg-slate-200 text-slate-900">
-                Completed
-              </Badge>
-            </div>
-          )}
+        {article.is_read && article.is_completed && (
+          <div className="flex justify-center">
+            <Badge className="relative m-auto -top-[11rem] text-md left-0 right-0 shadow-lg max-w-max bg-slate-200 text-slate-900">
+              Completed
+            </Badge>
+          </div>
+        )}
 
-          {article.is_approved && systemPathRegex.test(pathName) && (
-            <div className="flex justify-center">
-              <Badge className="relative m-auto -top-[11rem] text-md left-0 right-0 shadow-lg max-w-max bg-slate-200 text-slate-900">
-                Approved
-              </Badge>
-            </div>
-          )}
-        </Link>
+        {article.is_approved && systemPathRegex.test(pathName) && (
+          <div className="flex justify-center">
+            <Badge className="relative m-auto -top-[11rem] text-md left-0 right-0 shadow-lg max-w-max bg-slate-200 text-slate-900">
+              Approved
+            </Badge>
+          </div>
+        )}
       </div>
     );
   }
