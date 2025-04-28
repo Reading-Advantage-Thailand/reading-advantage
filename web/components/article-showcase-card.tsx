161f8d2fa2dbb6658055601756c349e445c4f855
OLEANG <wannachok.job@gmail.com>
2025-04-28 10:02:02 +0700

    fix: revert button function and remove read article from play button

--- web/components/article-showcase-card.tsx
+++ web/components/article-showcase-card.tsx
@@ -114,79 +114,82 @@ const ArticleShowcaseCard = React.forwardRef<HTMLDivElement, Props>(
                 >
                   ▶ Study as 45-min Lesson
                 </Link>
-                <Link
-                  href={`/student/read/${article.id}`}
-                  onClick={() =>
-                    fetch(`/api/v1/users/${userId}/activitylog`, {
-                      method: "POST",
-                      headers: { "Content-Type": "application/json" },
-                      body: JSON.stringify({
-                        articleId: article.id,
-                        activityType: ActivityType.ArticleRead,
-                        activityStatus: ActivityStatus.InProgress,
-                        details: {
-                          title: article.title,
-                          level: article.ra_level,
-                          cefr_level: article.cefr_level,
-                          type: article.type,
-                          genre: article.genre,
-                          subgenre: article.subgenre,
-                        },
-                      }),
-                    })
-                  }
-                  className="hover:bg-red-600 bg-red-800 text-[12px] font-semibold p-1 rounded-md"
-                >
-                  📖 Read article
-                </Link>
               </div>
             )}
           </div>
         </div>
-        <div
-          ref={ref}
-          className="w-full flex flex-col gap-1 h-[20rem] bg-cover bg-center p-3 rounded-md bg-black "
-          style={{
-            backgroundImage: `url('https://storage.googleapis.com/artifacts.reading-advantage.appspot.com/images/${article.id}.png')`,
-            boxShadow: "inset 80px 10px 90px 10px rgba(0, 0, 0, 0.9)",
-            opacity:
-              article.is_read ||
-              (article.is_approved && systemPathRegex.test(pathName))
-                ? 0.3
-                : 1,
-          }}
+        <Link
+          href={`/student/read/${article.id}`}
+          onClick={() =>
+            fetch(`/api/v1/users/${userId}/activitylog`, {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                articleId: article.id,
+                activityType: ActivityType.ArticleRead,
+                activityStatus: ActivityStatus.InProgress,
+                details: {
+                  title: article.title,
+                  level: article.ra_level,
+                  cefr_level: article.cefr_level,
+                  type: article.type,
+                  genre: article.genre,
+                  subgenre: article.subgenre,
+                },
+              }),
+            })
+          }
         >
-          {article.ra_level && (
+          <div
+            ref={ref}
+            className="w-full flex flex-col gap-1 h-[20rem] bg-cover bg-center p-3 rounded-md bg-black "
+            style={{
+              backgroundImage: `url('https://storage.googleapis.com/artifacts.reading-advantage.appspot.com/images/${article.id}.png')`,
+              boxShadow: "inset 80px 10px 90px 10px rgba(0, 0, 0, 0.9)",
+              opacity:
+                article.is_read ||
+                (article.is_approved && systemPathRegex.test(pathName))
+                  ? 0.3
+                  : 1,
+            }}
+          >
+            {article.ra_level && (
+              <Badge className="shadow-lg max-w-max" variant="destructive">
+                Reading Advantage Level: {article.ra_level}
+              </Badge>
+            )}
             <Badge className="shadow-lg max-w-max" variant="destructive">
-              Reading Advantage Level: {article.ra_level}
+              CEFR Level: {article.cefr_level}
             </Badge>
-          )}
-          <Badge className="shadow-lg max-w-max" variant="destructive">
-            CEFR Level: {article.cefr_level}
-          </Badge>
-          <Badge className="shadow-lg max-w-max" variant="destructive">
-            {t(article.genre)}, {t(article.subgenre)}
-          </Badge>
-          <Badge className="shadow-lg max-w-max" variant="destructive">
-            <Rating name="read-only" value={article.average_rating} readOnly />
-          </Badge>
-          <div className="mt-auto">
-            <div className=" bg-black bg-opacity-40">
-              <p className="text-xl drop-shadow-lg font-bold text-white">
-                {article.title}
-              </p>
-            </div>
-            <div className=" bg-black bg-opacity-40">
-              <p className="text-sm drop-shadow-lg line-clamp-4 text-white">
-                {locale == "en" ? (
-                  <React.Fragment>{article.summary}</React.Fragment>
-                ) : (
-                  <React.Fragment>{summarySentence}</React.Fragment>
-                )}
-              </p>
+            <Badge className="shadow-lg max-w-max" variant="destructive">
+              {t(article.genre)}, {t(article.subgenre)}
+            </Badge>
+            <Badge className="shadow-lg max-w-max" variant="destructive">
+              <Rating
+                name="read-only"
+                value={article.average_rating}
+                readOnly
+              />
+            </Badge>
+            <div className="mt-auto">
+              <div className=" bg-black bg-opacity-40">
+                <p className="text-xl drop-shadow-lg font-bold text-white">
+                  {article.title}
+                </p>
+              </div>
+              <div className=" bg-black bg-opacity-40">
+                <p className="text-sm drop-shadow-lg line-clamp-4 text-white">
+                  {locale == "en" ? (
+                    <React.Fragment>{article.summary}</React.Fragment>
+                  ) : (
+                    <React.Fragment>{summarySentence}</React.Fragment>
+                  )}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
+
         {article.is_read && !article.is_completed && (
           <div className="flex justify-center">
             <Badge className="relative m-auto -top-[11rem] text-md left-0 right-0 shadow-lg max-w-max bg-slate-200 text-slate-900">
