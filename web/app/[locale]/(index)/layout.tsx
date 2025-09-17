23855aea1d39cd96a0c5f9439ed56a9723392bb5
OLEANG <wannachok.job@gmail.com>
2025-09-17 20:26:52 +0700

    feat: Enhance layout and theme wrapper with improved background styles for better visual consistency

--- web/app/[locale]/(index)/layout.tsx
+++ web/app/[locale]/(index)/layout.tsx
@@ -16,25 +16,27 @@ export default async function Layout({ children }: { children: ReactNode }) {
 
   if (!user) {
     return (
-      <div className="flex min-h-screen flex-col">
-        <header className="container z-40">
-          <div className="flex h-20 items-center justify-between py-6">
-            <MainNav />
-            <nav>
-              <Link
-                href="/auth/signin"
-                className={cn(
-                  buttonVariants({ variant: "secondary", size: "sm" }),
-                  "px-4"
-                )}
-              >
-                {t("loginButton")}
-              </Link>
-            </nav>
-          </div>
-        </header>
-        <main className="flex-1">{children}</main>
-        <Footer />
+      <div className="bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+        <div className="flex min-h-screen flex-col bg-foreground/10 dark:bg-foreground/0">
+          <header className="container z-40 bg-background/45 rounded-b-3xl shadow-md">
+            <div className="flex h-20 items-center justify-between py-6">
+              <MainNav />
+              <nav>
+                <Link
+                  href="/auth/signin"
+                  className={cn(
+                    buttonVariants({ variant: "secondary", size: "sm" }),
+                    "px-4"
+                  )}
+                >
+                  {t("loginButton")}
+                </Link>
+              </nav>
+            </div>
+          </header>
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
       </div>
     );
   }
@@ -42,7 +44,7 @@ export default async function Layout({ children }: { children: ReactNode }) {
   if (user && user.cefr_level === "" && user.level === 0 && user.xp === 0) {
     return (
       <div className="bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
-        <div className="flex min-h-screen flex-col">
+        <div className="flex min-h-screen flex-col bg-foreground/10 dark:bg-foreground/0">
           <header className="container z-40 bg-background">
             <div className="flex h-20 items-center justify-between py-6">
               <MainNav />
@@ -59,8 +61,8 @@ export default async function Layout({ children }: { children: ReactNode }) {
   } else {
     return (
       <div className="bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
-        <div className="flex min-h-screen flex-col bg-foreground/10">
-          <header className="container z-40 ">
+        <div className="flex min-h-screen flex-col bg-foreground/10 dark:bg-foreground/0">
+          <header className="container z-40 bg-background/45 rounded-b-3xl shadow-md">
             <div className="flex h-20 items-center justify-between py-6">
               <MainNav />
               {/* <ProgressBar progress={user.xp} level={user.level!} /> */}
